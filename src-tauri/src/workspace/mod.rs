26cdb420c301d4c8957f7dec271383dfb5ba9814
Neelzee <nilsien2001@gmail.com>
2023-11-22 19:49:42 +0100

    fixed function that gets contents in folder

--- src-tauri/src/workspace/mod.rs
+++ src-tauri/src/workspace/mod.rs
@@ -1,8 +1,8 @@
 use std::sync::Mutex;
-use std::path::Path;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use walkdir::{WalkDir, DirEntry};
+use std::path::{Path, PathBuf};
 
 pub static ROOT_FOLDER: Lazy<Mutex<String>> = Lazy::new(|| {
     Mutex::new(String::from("."))
@@ -32,8 +32,15 @@ pub async fn set_root_folder(new_root: String) -> Option<()> {
 
 #[derive(Deserialize, Serialize, Debug)]
 pub enum FolderOrFile {
-    File { name: String, path: String },
-    Folder { name: String, path: String, contents: Vec<FolderOrFile> },
+    File {
+        name: String,
+        path: String
+    },
+    Folder {
+        name: String,
+        path: String,
+        contents: Vec<FolderOrFile>
+    },
 }
 
 
@@ -44,48 +51,26 @@ pub async fn get_content_in_folder(root: String) -> FolderOrFile {
     Err(_) => FolderOrFile::Folder { name: root.clone(), path: root, contents: Vec::new() },
     }
 }
+fn build_structure(path: &Path) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
+    let name = path.file_name().ok_or("No filename")?.to_string_lossy().into_owned();
+    let path_str = path.to_string_lossy().into_owned();
 
-pub fn get_all_files_and_folders(root: &str) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
-    let root_path = Path::new(root);
-    if !root_path.is_dir() {
-        return Err("Root path is not a directory".into());
-    }
-
-    fn build_structure(entry: DirEntry) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
-        let path = entry.path();
-        let name = entry.file_name().to_string_lossy().into_owned();
-
-        if path.is_dir() {
-            let mut contents = Vec::new();
-            for entry in WalkDir::new(path).min_depth(1).max_depth(1).into_iter().filter_map(|e| e.ok()) {
-                contents.push(build_structure(entry));
+    if path.is_dir() {
+        let mut contents = Vec::new();
+        for entry in WalkDir::new(path).min_depth(1).max_depth(1).into_iter().filter_map(|e| e.ok()) {
+            let entry_path = entry.path();
+            if entry_path == path {
+                continue; // Skip the root of the current iteration
             }
-            Ok(FolderOrFile::Folder { name, path: path.to_string_lossy().into(), contents: contents.into_iter().filter_map(|p| p.ok()).collect() })
-        } else {
-            Ok(FolderOrFile::File { name, path: path.to_string_lossy().into() })
+            contents.push(build_structure(entry_path)?);
         }
+        Ok(FolderOrFile::Folder { name, path: path_str, contents })
+    } else {
+        Ok(FolderOrFile::File { name, path: path_str })
     }
-
-    let con: Vec<FolderOrFile> = WalkDir::new(root_path)
-        .into_iter()
-        .fold(Vec::new(), |mut acc, el| {
-            if let Err(_) = el { return acc; }
-
-            if let Ok(r) = build_structure(el.unwrap()) {
-                acc.push(r);
-            }
-            acc
-        });
-
-    let mut root_name = root;
-
-    if let Some(fm) = root_path.file_name() {
-        if let Some(f) = fm.to_str() {
-            root_name = f;
-        }
-    }
-
-    Ok(FolderOrFile::Folder { name: root_name.to_owned(), path: root.to_owned(), contents: con })
-
 }
 
+pub fn get_all_files_and_folders(root: &str) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
+    let root_path = PathBuf::from(root);
+    build_structure(&root_path)
+}
