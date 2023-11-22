46a2a802cbeda2435965966513036962ca79095f
Neelzee <nilsien2001@gmail.com>
2023-11-22 11:25:34 +0100

    added get content from folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use crate::workspace::{get_root_folder, set_root_folder};
+use crate::workspace::{get_root_folder, set_root_folder, get_content_in_folder};
 
 mod workspace;
 
@@ -9,8 +9,9 @@ mod workspace;
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
-      get_root_folder,
-      set_root_folder
+      get_root_folder, // Gets root folder
+      set_root_folder, // Sets root folder
+      get_content_in_folder // Gets content from the given folder
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
--- src-tauri/src/workspace/mod.rs
+++ src-tauri/src/workspace/mod.rs
@@ -1,9 +1,11 @@
 use std::sync::Mutex;
-
+use std::path::Path;
 use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use walkdir::{WalkDir, DirEntry};
 
 pub static ROOT_FOLDER: Lazy<Mutex<String>> = Lazy::new(|| {
-    Mutex::new(String::new())
+    Mutex::new(String::from("."))
 });
 
 #[tauri::command]
@@ -25,5 +27,65 @@ pub async fn set_root_folder(new_root: String) -> Option<()> {
         return Some(());
     }
     None
+}
+
+
+#[derive(Deserialize, Serialize, Debug)]
+pub enum FolderOrFile {
+    File { name: String, path: String },
+    Folder { name: String, path: String, contents: Vec<FolderOrFile> },
+}
+
+
+#[tauri::command]
+pub async fn get_content_in_folder(root: String) -> FolderOrFile {
+    match get_all_files_and_folders(&root) {
+    Ok(res) => res,
+    Err(_) => FolderOrFile::Folder { name: root.clone(), path: root, contents: Vec::new() },
+    }
+}
+
+pub fn get_all_files_and_folders(root: &str) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
+    let root_path = Path::new(root);
+    if !root_path.is_dir() {
+        return Err("Root path is not a directory".into());
+    }
+
+    fn build_structure(entry: DirEntry) -> Result<FolderOrFile, Box<dyn std::error::Error>> {
+        let path = entry.path();
+        let name = entry.file_name().to_string_lossy().into_owned();
+
+        if path.is_dir() {
+            let mut contents = Vec::new();
+            for entry in WalkDir::new(path).min_depth(1).max_depth(1).into_iter().filter_map(|e| e.ok()) {
+                contents.push(build_structure(entry));
+            }
+            Ok(FolderOrFile::Folder { name, path: path.to_string_lossy().into(), contents: contents.into_iter().filter_map(|p| p.ok()).collect() })
+        } else {
+            Ok(FolderOrFile::File { name, path: path.to_string_lossy().into() })
+        }
+    }
+
+    let con: Vec<FolderOrFile> = WalkDir::new(root_path)
+        .into_iter()
+        .fold(Vec::new(), |mut acc, el| {
+            if let Err(_) = el { return acc; }
+
+            if let Ok(r) = build_structure(el.unwrap()) {
+                acc.push(r);
+            }
+            acc
+        });
+
+    let mut root_name = root;
+
+    if let Some(fm) = root_path.file_name() {
+        if let Some(f) = fm.to_str() {
+            root_name = f;
+        }
+    }
+
+    Ok(FolderOrFile::Folder { name: root_name.to_owned(), path: root.to_owned(), contents: con })
+
 }
 
