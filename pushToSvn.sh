0b13154711677b0f171f5c007bb5eed41e5c3009
Neelzee <nilsien2001@gmail.com>
2024-03-19 14:42:04 +0100

    Implemented push to svn script

--- pushToSvn.sh
+++ pushToSvn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Add new files
-svn add .
+# Removes unwanted files, it's easier than using svn:ignore
+rm -rf dist
+rm -rf node_modules
+rm -rf src-tauri/target
 
+# Add new files
+svn add . 
 # Commit changes
 svn commit -m "Update from GitLab CI" --username $SVN_USERNAME --password $SVN_PASSWORD --non-interactive
 
