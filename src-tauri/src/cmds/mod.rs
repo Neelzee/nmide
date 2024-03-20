0e949da850e371c4eb9b121cc5c0101557adff87
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-03-20 14:04:07 +0100

    started on backend calls

---
+++ src-tauri/src/cmds/mod.rs
@@ -0,0 +1,17 @@
+use crate::types::Fof;
+use eyre::{Context, Result};
+
+/// Gets workspace
+pub fn get_workspace(path: String) -> Result<Fof> {
+    todo!()
+}
+
+/// Saves the given content to the given file
+pub fn save_file(path: String, content: String) -> Result<()> {
+    todo!()
+}
+
+/// Closes the given file, without saving
+pub fn close_file(path: String) -> Result<()> {
+    todo!()
+}
