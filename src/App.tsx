187598d321f68fed0bf47f51e1c24daf6b9398b7
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-04-03 13:50:06 +0200

    FolderOrFiles is the issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { invoke } from "@tauri-apps/api";
 import { attachConsole } from "tauri-plugin-log-api";
 import ErrorPane from "./components/errorPane";
 import { createEffect, createSignal } from "solid-js";
-import { NmideReport, NmideError, FolderOrFile, Folder } from "./types";
+import { NmideReport, NmideError, FolderOrFile, Folder, File } from "./types";
 import { split_with_err } from "./funcs";
 import { produce } from "solid-js/store";
 
@@ -24,6 +24,7 @@ function App() {
       invoke<NmideError<FolderOrFile>>("get_workspace", { path: root() })
         .then(res => {
           console.log("START")
+          console.log(res);
           const [val, rep] = split_with_err<FolderOrFile>(res);
           if (rep !== null) {
             setErrors(produce(arr => {
@@ -42,12 +43,7 @@ function App() {
           }
         })
         .catch(err => {
-          console.log("ERROR")
           setErrors(produce(arr => arr.push(err)));
-        })
-        .finally(() => {
-          console.log(folders());
-          console.log(errors());
         });
     }
   });
@@ -55,11 +51,56 @@ function App() {
     <main>
       <ToolBar setRoot={setRoot} />
       <article>
-        <Explorer files={folders} />
+        <ul id={root()}>
+          <li>{folders().name}</li>
+          <ul>
+            {folders().content.map(fof => {
+              console.log(fof);
+              console.log((fof as Folder).content !== undefined);
+              if ("content" in fof) {
+                const sf = fof as Folder;
+                return <RenderFolder key={sf.path} folder={sf} />;
+              } else {
+                const sf = fof as File;
+                return <RenderFile key={sf.path} file={sf} />;
+              }
+            })}
+          </ul>
+        </ul>
         <ErrorPane errors={errors} />
       </article>
     </main>
   );
 }
+
+function RenderFile(props: { file: File, key: string }) {
+  const file = props.file;
+  return (
+    <ul class={`file ${file.extension} ${file.name}`} >
+      <li>{file.name}</li>
+    </ul>
+  );
+}
+
+function RenderFolder(props: { folder: Folder, key: string }) {
+  const folder = props.folder;
+  return (
+    <ul id={props.key}>
+      <li>{folder.name}</li>
+      <ul>
+        {folder.content.map(fof => {
+          if ("content" in fof) {
+            const sf = fof as Folder;
+            return <RenderFolder key={sf.path} folder={sf} />;
+          } else {
+            const sf = fof as File;
+            return <RenderFile key={sf.path} file={sf} />;
+          }
+        })}
+      </ul>
+    </ul>
+  );
+}
+
 
 export default App;
