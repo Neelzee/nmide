b0c461da3325f0b4df10ccf0384707dbfaa23c95
Neelzee <nilsien2001@gmail.com>
2024-07-30 21:02:15 +0200

    refactor!(html_lib): refactored html_lib
    test(html_lib): added simple_test for testing FFI
    
    BREAKING CHANGE: changed type-structure for CHtml

--- src-tauri/nmide-libc/html/html_lib.h
+++ src-tauri/nmide-libc/html/html_lib.h
@@ -20,28 +20,88 @@ typedef enum CElement {
   None,
 } CElement;
 
-/**
- * Struct representation of an HTML node
- **/
-typedef struct CHtmlNode {
-  CElement kind;
-  struct CHtmlNode *kids;
-  int kid_count;
-} CHtmlNode;
-
 typedef struct CHtmlText {
   char *text;
   int len;
 } CHtmlText;
 
 typedef union CHtmlUnion {
-  CHtmlNode node;
+  CElement kind;
   CHtmlText text;
 } CHtmlUnion;
 
 typedef struct CHtml {
   CHtmlUnion node;
   int isNode;
+  struct CHtml *kids;
+  int kid_count;
 } CHtml;
+
+/**
+ * Empty div
+ */
+CHtml div();
+
+/**
+ * Empty p
+ */
+CHtml p();
+
+/**
+ * Empty span
+ */
+CHtml span();
+
+/**
+ * Empty section
+ */
+CHtml section();
+
+/**
+ * Empty input
+ */
+CHtml input();
+
+/**
+ * Empty button
+ */
+CHtml button();
+
+/**
+ * Empty script
+ */
+CHtml script();
+
+/**
+ * Empty select
+ */
+CHtml select();
+
+/**
+ * Empty aside
+ */
+CHtml aside();
+
+/**
+ * Empty nav
+ */
+CHtml nav();
+
+/**
+ * Empty a
+ */
+CHtml a();
+
+/**
+ * Empty text
+ */
+CHtml text();
+
+/**
+ * <div>
+ *  <p>Hello, World!</p>
+ * </div>
+ */
+CHtml simple_test();
 
 #endif // !HTML_LIB
