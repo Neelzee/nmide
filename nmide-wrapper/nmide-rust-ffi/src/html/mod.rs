ba2b3ad7ed0fec6b2f09c0c3020266d2eb673bfa
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-08-08 14:24:49 +0200

    refactor!(nmide-rust-ffi): Html is now enum

--- nmide-wrapper/nmide-rust-ffi/src/html/mod.rs
+++ nmide-wrapper/nmide-rust-ffi/src/html/mod.rs
@@ -3,12 +3,14 @@ use std::ffi::{CStr, CString};
 
 use anyhow::{Context, Result};
 use c_vec::CVec;
-use safer_ffi::c_char;
+use safer_ffi::{c_char, prelude::AsOut};
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
 use crate::{
-    CHtml, CHtmlContent, CHtmlElement, CHtmlTag, CHtmlTag_A, CHtmlTag_Aside, CHtmlTag_Button, CHtmlTag_Div, CHtmlTag_Input, CHtmlTag_Nav, CHtmlTag_None, CHtmlTag_P, CHtmlTag_Script, CHtmlTag_Section, CHtmlTag_Select, CHtmlTag_Span
+    CHtml, CHtmlContent, CHtmlElement, CHtmlTag, CHtmlTag_A, CHtmlTag_Aside, CHtmlTag_Button,
+    CHtmlTag_Div, CHtmlTag_Input, CHtmlTag_Nav, CHtmlTag_None, CHtmlTag_P, CHtmlTag_Script,
+    CHtmlTag_Section, CHtmlTag_Select, CHtmlTag_Span, CHtmlText,
 };
 
 #[cfg(test)]
@@ -68,21 +70,14 @@ impl Element {
     }
 }
 
-#[derive(Debug, TS, Serialize, Deserialize, PartialEq, Eq)]
-#[ts(export)]
-pub struct Html {
-    kind: Element,
-    kids: Vec<Html>,
-}
-
 #[derive(Debug, TS, Serialize, Deserialize, PartialEq, Eq, Clone)]
 #[ts(export)]
-pub enum RHtml {
-    Div { kids: Vec<RHtml> },
+pub enum Html {
+    Div { kids: Vec<Html> },
     Text(String),
 }
 
-impl RHtml {
+impl Html {
     pub fn from_c(chtml: CHtml) -> Result<Self> {
         if chtml.isElement {
             let element: CHtmlElement = unsafe { *(*chtml.content).element };
@@ -97,20 +92,72 @@ impl RHtml {
                 if !ptr.is_null() {
                     kids.push(Self::from_c(unsafe { *ptr })?);
                 }
-
             }
 
             match Element::from_c(element.tag) {
                 Element::Div => Ok(Self::Div { kids }),
                 _ => unimplemented!("TODO"),
             }
-
         } else {
-            Ok(Self::Text(from_char(unsafe { (*(*chtml.content).text).text.clone() })?))
+            Ok(Self::Text(from_char(unsafe {
+                (*(*chtml.content).text).text.clone()
+            })?))
+        }
+    }
+    pub fn to_c(self) -> Result<CHtml> {
+        match self {
+            Html::Text(s) => Ok(CHtml {
+                content: CHtmlContent {
+                    text: CHtmlText {
+                        text: to_char(&s)?,
+                        len: s.chars().count(),
+                    }
+                    .as_out()
+                    .as_mut_ptr(),
+                }
+                .as_out()
+                .as_mut_ptr(),
+                isElement: false,
+            }),
+            Html::Div { kids } => {
+                let len = kids.len();
+                Ok(CHtml {
+                    content: CHtmlContent {
+                        element: CHtmlElement {
+                            tag: CHtmlTag_Div,
+                            children: kids
+                                .into_iter()
+                                .map(|k| k.to_c())
+                                .filter(|k| k.is_ok())
+                                .map(|k| k.unwrap())
+                                .collect::<Vec<CHtml>>()
+                                .as_out()
+                                .as_mut_ptr()
+                                .as_out()
+                                .as_mut_ptr(),
+                            len,
+                        }
+                        .as_out()
+                        .as_mut_ptr(),
+                    }
+                    .as_out()
+                    .as_mut_ptr(),
+                    isElement: true,
+                })
+            }
         }
     }
 }
 
 pub fn from_char(c: *mut i8) -> Result<String> {
-    Ok(unsafe {CStr::from_ptr(c)}.to_str()?.to_string())
+    Ok(unsafe { CStr::from_ptr(c) }.to_str()?.to_string())
+}
+
+pub fn to_char(s: &str) -> Result<*mut i8> {
+    Ok(CString::new(s)?
+        .into_bytes_with_nul()
+        .into_iter()
+        .map(|c| c as i8)
+        .collect::<Vec<i8>>()
+        .as_mut_ptr())
 }
