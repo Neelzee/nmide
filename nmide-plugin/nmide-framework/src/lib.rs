059b9eaa35dcebcf593dd1d7313c95ede1c6f2a0
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-08-09 11:18:13 +0200

    refactor!(nmide-plugin): simplified framework plugin

--- nmide-plugin/nmide-framework/src/lib.rs
+++ nmide-plugin/nmide-framework/src/lib.rs
@@ -1,19 +1,17 @@
-use nmide_rust_ffi::{
-    html::{Element, Html},
-    CHtml,
-};
+use std::ptr::null_mut;
+
+use nmide_rust_ffi::{html::Html, CHtml, CModel};
 
 #[no_mangle]
-pub extern "C" fn view() -> CHtml {
-    Html::new(
-        Element::Div,
-        vec![Html::new(
-            Element::P,
-            vec![Html::new(
-                Element::Text("Hello, World!".to_string()),
-                Vec::new(),
-            )],
-        )],
-    )
+pub extern "C" fn view(_: CModel) -> CHtml {
+    Html::Div {
+        kids: vec![Html::Text("Hello, World!".to_string())],
+    }
     .to_c()
+    .unwrap_or_default()
+}
+
+#[no_mangle]
+pub extern "C" fn manifest() -> CModel {
+    CModel { map: null_mut() }
 }
