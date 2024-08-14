fb19afd35b3ce4cd8c8d0f616f063c159b8bd039
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-08-14 22:36:27 +0200

    feat(nmide-framework): added counter example

--- nmide-plugin/nmide-framework/src/lib.rs
+++ nmide-plugin/nmide-framework/src/lib.rs
@@ -1,19 +1,24 @@
 use nmide_rust_ffi::{
     attr::Attr,
     html::Html,
+    map::Value,
     model::{Model, Msg},
 };
 
 #[no_mangle]
-pub extern "Rust" fn view(_: Model) -> Html {
+pub extern "Rust" fn view(model: Model) -> Html {
+    let count = match model.lookup("counter") {
+        Some(Value::Int(x)) => x.to_string(),
+        _ => "No count found :(".to_string(),
+    };
+
     Html::Div {
         kids: vec![
             Html::Text("Hello, World!".to_string()),
-            Html::Div {
-                kids: Vec::new(),
-                attrs: vec![Attr::OnClick(Msg::PluginMsg(
-                    "Message Says: Howdy!".to_string(),
-                ))],
+            Html::Text(format!("{count}")),
+            Html::Btn {
+                kids: vec![Html::Text("Click".to_string())],
+                attrs: vec![Attr::OnClick(Msg::PluginMsg("counter".to_string()))],
             },
         ],
         attrs: Vec::new(),
@@ -21,7 +26,26 @@ pub extern "Rust" fn view(_: Model) -> Html {
 }
 
 #[no_mangle]
+pub extern "Rust" fn init() -> Model {
+    vec![("counter", 0)].into()
+}
+
+#[no_mangle]
+pub extern "Rust" fn update(msg: Msg, model: Model) -> Model {
+    match msg {
+        Msg::PluginMsg(s) => match s.as_str() {
+            "counter" => match model.lookup("counter") {
+                Some(Value::Int(x)) => model.insert("counter", Value::Int(x + 1)),
+                _ => model,
+            },
+
+            _ => model,
+        },
+    }
+}
+
+#[no_mangle]
 pub extern "Rust" fn manifest() -> Model {
-    let funcs: Model = vec![("functions", vec!["view"])].into();
+    let funcs: Model = vec![("functions", vec!["view", "update", "init"])].into();
     funcs.merge(vec![("rust", 1)].into())
 }
