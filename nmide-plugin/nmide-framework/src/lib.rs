36f477f3ea2ef3e1b00c61535b802039efe0acfa
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-08-14 19:49:56 +0200

    refactor!(nmide-framework): changed plugin to rust

--- nmide-plugin/nmide-framework/src/lib.rs
+++ nmide-plugin/nmide-framework/src/lib.rs
@@ -1,17 +1,27 @@
-use std::ptr::null_mut;
-
-use nmide_rust_ffi::{html::Html, CHtml, CModel};
+use nmide_rust_ffi::{
+    attr::Attr,
+    html::Html,
+    model::{Model, Msg},
+};
 
 #[no_mangle]
-pub extern "C" fn view(_: CModel) -> CHtml {
+pub extern "Rust" fn view(_: Model) -> Html {
     Html::Div {
-        kids: vec![Html::Text("Hello, World!".to_string())],
+        kids: vec![
+            Html::Text("Hello, World!".to_string()),
+            Html::Div {
+                kids: Vec::new(),
+                attrs: vec![Attr::OnClick(Msg::PluginMsg(
+                    "Message Says: Howdy!".to_string(),
+                ))],
+            },
+        ],
+        attrs: Vec::new(),
     }
-    .to_c()
-    .unwrap_or_default()
 }
 
 #[no_mangle]
-pub extern "C" fn manifest() -> CModel {
-    CModel { map: null_mut() }
+pub extern "Rust" fn manifest() -> Model {
+    let funcs: Model = vec![("functions", vec!["view"])].into();
+    funcs.merge(vec![("rust", 1)].into())
 }
