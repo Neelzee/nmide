f183ae9d212847dabb019632184c906fddae69d9
Neelzee <nilsien2001@gmail.com>
2024-08-26 17:23:25 +0200

    refactor!(nmide-plugin-manager): plugin refactor
    Plugins, (Nmlugins), must now have init, view, and
    update, functions. So removed old code which
    allowed plugins not to have those. Manifest is now
    a third-party-plugin concept, not a part of std.
    nmide plugin architecture.

--- nmide-core/src-tauri/nmide-plugin-manager/src/lib.rs
+++ nmide-core/src-tauri/nmide-plugin-manager/src/lib.rs
@@ -1,10 +1,9 @@
-use anyhow::{anyhow, Context, Result};
-use either::Either;
+use anyhow::{Context, Result};
 use libloading::{self, Library, Symbol};
 use nmide_std_lib::{
     html::Html,
-    interface::rfunctions::{RInit, RManifest, RUpdate, RView},
-    map::{value::Value, Map},
+    interface::rfunctions::{RInit, RUpdate, RView},
+    map::Map,
     msg::Msg,
 };
 use std::{ffi::OsStr, path::PathBuf};
@@ -12,10 +11,6 @@ use std::{ffi::OsStr, path::PathBuf};
 #[derive(Debug)]
 pub struct Nmlugin {
     lib: Library,
-    init_fn: Option<Either<(), ()>>,
-    view_fn: Option<Either<(), ()>>,
-    update_fn: Option<Either<(), ()>>,
-    manifest: Map,
     path: PathBuf,
 }
 
@@ -24,111 +19,30 @@ impl Nmlugin {
     where
         P: AsRef<OsStr>,
     {
-        let lib = unsafe { Library::new(&path) }.context("Failed loading plugin")?;
-        let _manifest: Symbol<RManifest> = unsafe {
-            lib.get(b"manifest")
-                .context("Failed loading plugin, no manifest")
-        }?;
-        let manifest = unsafe { _manifest() };
-        let mut init_fn = None;
-        let mut view_fn = None;
-        let mut update_fn = None;
-        let is_rust = manifest
-            .lookup("nmide-plugin-type")
-            .is_some_and(|a| match a {
-                Value::String(x) if x.to_lowercase() == "rust" => true,
-                _ => false,
-            });
-        match manifest.lookup("nmide-functions") {
-            Some(Value::List(funcs)) => {
-                for func in funcs {
-                    match func {
-                        Value::String(x) => match x.to_lowercase().as_str() {
-                            "init" => {
-                                if is_rust {
-                                    init_fn = Some(Either::Right(()));
-                                } else {
-                                    init_fn = Some(Either::Left(()));
-                                }
-                            }
-                            "view" => {
-                                if is_rust {
-                                    view_fn = Some(Either::Right(()));
-                                } else {
-                                    view_fn = Some(Either::Left(()));
-                                }
-                            }
-                            "update" => {
-                                if is_rust {
-                                    update_fn = Some(Either::Right(()));
-                                } else {
-                                    update_fn = Some(Either::Left(()));
-                                }
-                            }
-                            _ => continue,
-                        },
-                        _ => continue,
-                    }
-                }
-            }
-            _ => todo!(),
-        }
         Ok(Self {
-            lib,
-            init_fn,
-            view_fn,
-            update_fn,
-            manifest,
+            lib: unsafe { Library::new(&path) }.context("Failed loading plugin")?,
             path: PathBuf::from(&path),
         })
     }
 
     pub fn init(&self) -> Result<Map> {
-        match self.init_fn {
-            Some(Either::Left(_)) => {
-                unimplemented!()
-            }
-            Some(Either::Right(_)) => {
-                let _init: Symbol<RInit> =
-                    unsafe { self.lib.get(b"init") }.context("Failed getting Rust `init`")?;
-                Ok(unsafe { _init() })
-            }
-            _ => Err(anyhow!("No `init` in plugin")),
-        }
+        let _init: Symbol<RInit> =
+            unsafe { self.lib.get(b"init") }.context("Failed getting Rust `init`")?;
+        Ok(unsafe { _init() })
     }
 
     pub fn view(&self, model: Map) -> Result<Html> {
-        match self.view_fn {
-            Some(Either::Left(_)) => {
-                unimplemented!()
-            }
-            Some(Either::Right(_)) => {
-                let _view: Symbol<RView> =
-                    unsafe { self.lib.get(b"view") }.context("Failed getting Rust `view`")?;
+        let _view: Symbol<RView> =
+            unsafe { self.lib.get(b"view") }.context("Failed getting Rust `view`")?;
 
-                Ok(unsafe { _view(model) })
-            }
-            _ => Err(anyhow!("No `view` in plugin")),
-        }
+        Ok(unsafe { _view(model) })
     }
 
     pub fn update(&self, msg: Msg, model: Map) -> Result<Map> {
-        match self.update_fn {
-            Some(Either::Left(_)) => {
-                unimplemented!()
-            }
-            Some(Either::Right(_)) => {
-                let _update: Symbol<RUpdate> =
-                    unsafe { self.lib.get(b"update") }.context("Failed getting Rust `update`")?;
+        let _update: Symbol<RUpdate> =
+            unsafe { self.lib.get(b"update") }.context("Failed getting Rust `update`")?;
 
-                Ok(unsafe { _update(msg, model) })
-            }
-            _ => Err(anyhow!("No `update` in plugin")),
-        }
-    }
-
-    pub fn manifest(&self) -> &Map {
-        &self.manifest
+        Ok(unsafe { _update(msg, model) })
     }
 
     pub fn path(&self) -> &PathBuf {
