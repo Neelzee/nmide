541afeb9c51ebbebf8c7013adbdbd523a7d1defc
Neelzee <nilsien2001@gmail.com>
2024-09-01 20:21:25 +0200

    test(nmide-core): corrected tests
    Corrected tests to fit the updated api

--- nmide-core/src/__tests__/render_html_simple.test.ts
+++ nmide-core/src/__tests__/render_html_simple.test.ts
@@ -1,22 +1,16 @@
 import { expect, test } from 'vitest';
-import RenderHtml from "../components/Html";
+import RenderHtml from '../components/Html';
 import { render, screen } from '@testing-library/react'
 
 test("rendering with text should display text", () => {
   render(
-    <RenderHtml
-      html={
-        {
-          kind: "Div",
-          kids: [
-            {
-              kind: { "Text": "Hello, World!" },
-              kids: []
-            }
-          ]
-        }
-      }
-    />,
+    RenderHtml(
+      {
+        kind: "Div",
+        attrs: [],
+        kids: [],
+        text: "Hello, World!"
+      }),
   );
 
   const txt = screen.getByText("Hello, World!").textContent;
