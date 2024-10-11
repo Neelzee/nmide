f97b5023dab7fb1aae4f127e13484ab1d7f113e5
Nils.Fitjar <nfi005@uib.no>
2024-10-11 12:44:29 +0200

    refactor(nmide-plugin): removed deprecated exs.

--- nmide-plugin/src/lib.rs
+++ nmide-plugin/src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use abi_stable::{
+    export_root_module,
+    prefix_type::PrefixTypeTrait,
+    rvec, sabi_extern_fn,
+    std_types::{ROption, RString, RVec},
+};
+use nmide_std_lib::{
+    attr::rattr::RAttr,
+    html::rhtml::RHtml,
+    map::rmap::RMap,
+    msg::rmsg::{RMsg, RMsgKind, RMsgUnion},
+    NmideStandardLibrary_Ref, NmideStdLib,
+};
+use std::str::FromStr;
+
+#[export_root_module]
+pub fn get_library() -> NmideStandardLibrary_Ref {
+    NmideStdLib { init, view, update }.leak_into_prefix()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[sabi_extern_fn]
+pub fn init() -> RMap {
+    RMap::new().insert("counter", 0)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[sabi_extern_fn]
+pub fn view(model: RMap) -> RHtml {
+    let count = model
+        .lookup("counter")
+        .and_then(|v| ROption::RSome(v.int().unwrap_or_default()))
+        .unwrap_or_default();
+    RHtml::Div(
+        rvec![
+            RHtml::text(RString::from_str(&format!("Count: {count}")).unwrap_or_default()),
+            RHtml::Button(
+                RVec::new(),
+                rvec![RAttr::new_click(RMsg::new(
+                    RMsgKind::Msg,
+                    RMsgUnion::new(RString::from_str("increment").unwrap_or_default(), 1.into())
+                ))]
+            )
+        ],
+        RVec::new(),
+    )
+}
+
+#[sabi_extern_fn]
+pub fn update(msg: RMsg, model: RMap) -> RMap {
+    if !msg.is_msg("increment") {
+        return model;
     }
+    let count = model
+        .lookup("counter")
+        .map(|v| v.int().unwrap_or_default())
+        .unwrap();
+    model.insert("counter", count + 1)
 }
