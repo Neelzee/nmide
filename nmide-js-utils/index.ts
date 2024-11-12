b8f82d3befd3ad1263b3f84c015103ddae308076
Neelzee <nilsien2001@gmail.com>
2024-11-12 20:10:00 +0100

    chore(nmide-js-utils): init commit

---
+++ nmide-js-utils/index.ts
@@ -0,0 +1,13 @@
+import * as HtmlBuilder from "./HtmlBuilder";
+import * as TAttr from "./bindings/TAttr";
+import * as THtml from "./bindings/THtml";
+import * as TMap from "./bindings/TMap";
+import * as TMsg from "./bindings/TMsg";
+
+export {
+  HtmlBuilder,
+  TAttr,
+  THtml,
+  TMap,
+  TMsg,
+};
