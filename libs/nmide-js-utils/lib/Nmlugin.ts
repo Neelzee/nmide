982da75f071d2b8694ee0953304273f51760ad5a
Neelzee <nilsien2001@gmail.com>
2024-11-16 19:55:32 +0100

    feat(nmide-js-utils): Added async plugin interface

--- libs/nmide-js-utils/lib/Nmlugin.ts
+++ libs/nmide-js-utils/lib/Nmlugin.ts
@@ -14,3 +14,30 @@ export interface NmluginVerified {
   update: (msg: TMsg, model: TMap) => TMap;
 };
 
+export interface AsyncNmluginUnknown {
+  init: () => Promise<unknown>,
+  view: (model: TMap) => Promise<unknown>;
+  update: (msg: TMsg, model: TMap) => Promise<unknown>;
+}
+
+export interface AsyncNmluginVerified {
+  init: () => Promise<TMap>,
+  view: (model: TMap) => Promise<THtml>;
+  update: (msg: TMsg, model: TMap) => Promise<TMap>;
+}
+
+export const toAsyncUnknown = (nmlugin: NmluginUnknown): AsyncNmluginUnknown => {
+  return {
+    init: () => new Promise(nmlugin.init),
+    view: (model: TMap) => new Promise(() => nmlugin.view(model)),
+    update: (msg: TMsg, model: TMap) => new Promise(() => nmlugin.update(msg, model)),
+  };
+}
+
+export const toAsyncVerified = (nmlugin: NmluginVerified): AsyncNmluginVerified => {
+  return {
+    init: () => new Promise(nmlugin.init),
+    view: (model: TMap) => new Promise(() => nmlugin.view(model)),
+    update: (msg: TMsg, model: TMap) => new Promise(() => nmlugin.update(msg, model)),
+  };
+}
