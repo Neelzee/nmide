f2c073d1a080d69fed8dab2a9cbfa733d5ddde5d
Nils.Fitjar <nfi005@uib.no>
2024-11-16 14:43:44 +0100

    chore(mini-lsp-client): init commit

---
+++ plugins/mini-lsp-client/src/Effect/InstallPlugin.purs
@@ -0,0 +1,11 @@
+module Effect.InstallPlugin
+  ( installPlugin
+  )
+  where
+
+import Prelude
+
+import Effect (Effect)
+import Plugins (Plugin)
+
+foreign import installPlugin :: String -> Plugin -> Effect Unit
