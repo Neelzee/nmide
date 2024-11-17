6a3884d81c7b891d8b6fb372d9ce96bc2d500de7
Nils.Fitjar <nfi005@uib.no>
2024-11-17 14:17:45 +0100

    feat!(nmide-plugin-manager): Updated plugin-sig
    Init, Update and View now take in a reference to Msg and Model, instead
    of the value.
    
    BREAKING CHANGE: Signature change for Nmlugin

--- core/src-tauri/nmide-plugin-manager/src/lib.rs
+++ core/src-tauri/nmide-plugin-manager/src/lib.rs
@@ -1,23 +1,18 @@
-use abi_stable::library::{LibraryPath, RootModule};
-use anyhow::Result;
+use abi_stable::library::{LibraryError, LibraryPath, RootModule};
 use nmide_std_lib::{
-    html::rhtml::RHtml, map::rmap::RMap, msg::rmsg::RMsg, NmideStandardLibrary_Ref,
+    html::rhtml::RHtml, map::rmap::RMap, msg::rmsg::RMsg, NmideStandardLibraryRef,
 };
 use std::path::Path;
 
 pub struct Nmlugin {
-    lib: NmideStandardLibrary_Ref,
+    lib: NmideStandardLibraryRef,
     plugin_path: String,
 }
 
 impl Nmlugin {
-    pub fn new(path: &Path) -> Result<Self> {
-        let pp = LibraryPath::FullPath(path);
-        //let plugin = NmideStandardLibrary_Ref::load_from_directory(&plugin_path)?;
-        let plugin = NmideStandardLibrary_Ref::load_from(pp)?;
-
+    pub fn new(path: &Path) -> Result<Self, LibraryError> {
         Ok(Self {
-            lib: plugin,
+            lib: NmideStandardLibraryRef::load_from(LibraryPath::FullPath(path))?,
             plugin_path: path.to_string_lossy().to_string(),
         })
     }
@@ -26,15 +21,19 @@ impl Nmlugin {
         self.lib.init()()
     }
 
-    pub fn view(&self, model: RMap) -> RHtml {
+    pub fn view(&self, model: &RMap) -> RHtml {
         self.lib.view()(model)
     }
 
-    pub fn update(&self, msg: RMsg, model: RMap) -> RMap {
+    pub fn update(&self, msg: &RMsg, model: &RMap) -> RMap {
         self.lib.update()(msg, model)
     }
 
     pub fn name(&self) -> &str {
+        self.plugin_path.split("/").last().unwrap_or_default()
+    }
+
+    pub fn path(&self) -> &str {
         &self.plugin_path
     }
 }
