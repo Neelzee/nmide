1541a718327fe5082902b01bc2ec042863f2026c
Nils.Fitjar <nfi005@uib.no>
2024-11-24 15:26:34 +0100

    chore(core): Updated Plugins

--- core/plugins/reggub_helper.js
+++ core/plugins/reggub_helper.js
@@ -4,34 +4,10 @@ window.plugins.set(
     init: () => {
       return [];
     },
-    update: (msg, model) => {
+    update: (_, __) => {
       return [];
     },
-    view: model => {
-      const openTab = (tab) => {
-        // Declare all variables
-        var i, tabcontent, tablinks;
-
-        // Get all elements with class="tabcontent" and hide them
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-        }
-
-        // Get all elements with class="tablinks" and remove the class "active"
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(tab).style.display = "block";
-      };
-      const val = model.find(([k, _]) => k === "reggub-tab-btn");
-      if (val !== undefined) {
-        const tab = val[1].Str;
-        openTab(tab);
-      }
+    view: _ => {
       return {
         kind: "Frag",
         kids: [],
@@ -39,5 +15,24 @@ window.plugins.set(
         attrs: [],
       }
     },
+    openTab: (tab) => {
+      // Declare all variables
+      var i, tabcontent, tablinks;
+
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+
+      // Get all elements with class="tablinks" and remove the class "active"
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(tab).style.display = "block";
+    }
   }
 );
