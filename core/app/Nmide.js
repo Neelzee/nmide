9f343157c7079be8a95c30e477a7b8bc0f40d736
Nils.Fitjar <nfi005@uib.no>
2024-11-27 14:50:02 +0100

    feat(core): Partial replacement of Main
    Main.ts is partially replaced with Main.purs, added PureScript build to
    Tauri

---
+++ core/app/Nmide.js
@@ -0,0 +1,9 @@
+import { App } from "@/app/App";
+
+export const app = () => App();
+
+export const debug = (a) => () => console.debug(a);
+
+export const debugJson = (a) => () => console.debug(a);
+
+export const debugEither = (a) => () => console.debug(a);
