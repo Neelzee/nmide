7b1b5c0416c4c360232422592d80af1b6a8f4858
Nils.Fitjar <nfi005@uib.no>
2024-11-28 15:34:49 +0100

    fix(nmide-std-lib): Added main tag

--- core/src-tauri/nmide-std-lib/src/html/mod.rs
+++ core/src-tauri/nmide-std-lib/src/html/mod.rs
@@ -34,7 +34,7 @@ pub mod thtml {
     thtmlkind!(
         Div, P, H1, H2, H3, H4, H5, H6, Span, Section, Article, Aside, Audio, B, Br, Button, Code,
         Em, Fieldset, Form, Img, Input, Label, Link, Li, Menu, Nav, Ol, Option, Select, Style, Svg,
-        Table, Td, Th, Tr, Ul, Video, Frag, Text, Script, Tbody
+        Table, Td, Th, Tr, Ul, Video, Frag, Text, Script, Tbody, Main
     );
 
     #[derive(Serialize, Deserialize, TS)]
