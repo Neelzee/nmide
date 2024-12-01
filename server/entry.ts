54615d0b054eef6c0d0f9c5718958f1e3833879a
Neelzee <nilsien2001@gmail.com>
2024-12-01 20:34:39 +0100

    feat(server): setup now takes options
    Server-IDE is *almost* done.

--- server/entry.ts
+++ server/entry.ts
@@ -1,5 +1,37 @@
 import { main } from "./index.js";
 import { setup } from "./setup.js";
+import * as E from "fp-ts/Either";
 
-setup();
+let app = null;
+
+setup({
+  root: document.body,
+  client: (x?: any, ..._: any) => {
+    if (x === undefined) return new Promise(r => r({}));
+    if (x === "init") return new Promise(r => r(E.right([])));
+    if (x === "view") return new Promise(r => r(E.right([])));
+    if (x === "update") return new Promise(r => r(E.right([])));
+  },
+  listen: (x?: any, ...y: any) => new Promise(r => {
+    console.log(x, ...y);
+    if (x === "msg" && typeof y === "function" && app === null) {
+      app = y;
+      return r({});
+    }
+    if (x === "msg" && typeof y === "object") {
+      // @ts-ignore
+      app(y);
+    }
+    return r({});
+  }),
+  emit: (...x: any) => {
+    console.log(x);
+    return new Promise(r => r({}));
+  },
+  getPluginPaths: new Promise(r => r([
+    "./plugins/ide-view.js",
+    "./plugins/ide-framework.js",
+    "./plugins/counter.js",
+  ])),
+});
 main();
--- server/startServer.sh
+++ server/startServer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+rm *.js
+cd ../core && node build.js
+cd ../server
+cp ../core/build/index.js .
+cp ../core/build/app/setup.js .
 npm run dev &
 cargo run
