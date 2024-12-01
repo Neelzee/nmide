12d7f9b67c5ad2024aee98e047f49f2cbc1512e1
Nils.Fitjar <nfi005@uib.no>
2024-12-01 10:20:39 +0100

    refactor(dependency-viewer): now utilizes IDE-PS
    The IDE-Plugin-System, is the designation for the IDE-* plugins, which,
    combined should lead to an IDE.

--- plugins/dependency-viewer/main.ts
+++ plugins/dependency-viewer/main.ts
@@ -1,19 +1,14 @@
 import {
-  HtmlBuilder,
   THtml,
-  tLookup,
   TMap,
   TMsg,
-  tObj,
-  TValueBool,
-  TValueObj,
-  TValueStr
+  MapBuilder,
+  emptyHtml,
+  TValueStr,
+  tStr,
+  tLookupOr
 } from "@nmide/js-utils"
-import MapBuilder from "@nmide/js-utils/lib/MapBuilder";
-import { map, getOrElse } from "fp-ts/Option";
-import { pipe } from "fp-ts/lib/function";
 
-// @ts-ignore
 window.plugins.set(
   "DependencyViewer",
   {
@@ -23,48 +18,15 @@ window.plugins.set(
         .build();
     },
     view: (model: TMap): THtml => {
-      return new HtmlBuilder()
-        .kids([
-          new HtmlBuilder()
-            .kind("button")
-            .text("Render Dependency")
-            .attrs([{
-              onClick: {
-                msg: ["dependency_render", {
-                  str: pipe(
-                    model,
-                    tLookup<TValueStr>("info-module-graph"),
-                    map<TValueStr, string>(a => a.str),
-                    getOrElse(() => {
-                      return "";
-                    })
-                  )
-                }]
-              }
-            }])
-        ])
-        .build();
-    },
-    update: (msg: TMsg, model: TMap): TMap => {
-      if (msg.msg[0] !== "dependency_render") return [];
-      const skip = pipe(
-        model,
-        tLookup<TValueBool>("DependencyViewerInit"),
-        map<TValueBool, boolean>(a => a.bool),
-        getOrElse(() => false),
-      );
-      if (skip) return [];
-      const graph = pipe(
-        model,
-        tLookup<TValueObj>("info-module-graph"),
-        getOrElse(() => tObj([])),
-      );
-      const div = document.createElement("div");
+      const graph = tLookupOr<TValueStr>("info-module-graph")(tStr(""))(model);
+      let div = document.getElementById("graph");
+      if (div === null) div = document.createElement("div");
       div.id = "graph";
-      document.body.appendChild(div);
-      //@ts-ignore
-      window.plugins.get("DependencyViewerHelper").render(graph);
-      return [];
+      div.textContent = "";
+      window.root.appendChild(div);
+      window.plugins.get("DependencyViewerHelper").render(graph.str);
+      return emptyHtml();
     },
+    update: (_: TMsg, __: TMap): TMap => [],
   }
 );
