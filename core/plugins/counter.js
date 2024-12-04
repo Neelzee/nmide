0cc4843eecd892c6ebb22f17e28de91e4cd51035
Nils.Fitjar <nfi005@uib.no>
2024-12-04 15:08:46 +0100

    chore(core): updated plugins

---
+++ core/plugins/counter.js
@@ -0,0 +1,48 @@
+window.plugins.set(
+  "COUNTER-PLUGIN",
+  {
+    init: () => {
+      return [["counter", { "int": 0 }]];
+    },
+    update: (msg, model) => {
+      if (msg.msg[0] === "increment") {
+        const prevVal = model.find(([k, _]) => k === "counter")[1].int;
+        const increment = msg.msg[1]["int"];
+        return [
+          ["counter",
+            {
+              "int": prevVal === undefined
+                ? 0
+                : prevVal + (increment === undefined ? 1 : increment)
+            }
+          ]
+        ];
+      }
+      return model;
+    },
+    view: model => {
+      const count = model.find(([k, _]) => k === "counter");
+      return {
+        kind: "div",
+        kids: [
+          {
+            kind: "text",
+            kids: [],
+            text: `counter: ${count === undefined ? "No Count found" : count[1]["int"]}`,
+            attrs: [],
+          },
+          {
+            kind: "button",
+            kids: [],
+            text: "click",
+            attrs: [
+              { "onClick": { "msg": ["increment", { "int": 1 }] } }
+            ],
+          }
+        ],
+        text: null,
+        attrs: [],
+      }
+    },
+  }
+);
