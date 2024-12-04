954687e4f11bafdba66aa0b6c7c259d4db75184d
Nils.Fitjar <nfi005@uib.no>
2024-12-04 15:39:28 +0100

    feat(server): Plugins work on server

--- server/entry.ts
+++ server/entry.ts
@@ -1,37 +1,37 @@
-import { main } from "./index.js";
-import { setup } from "./setup.js";
+import { App } from "../core/app/App";
+import { renderHtml, parseHtml } from "../core/app/lib/renderHtml";
+import { jspInstaller } from "../core/app/lib/jspInstaller";
+import { cssInstaller } from "../core/app/lib/cssInstaller";
 import * as E from "fp-ts/Either";
 
-let app = null;
+const listenHandler: Map<string, any> = new Map();
 
-setup({
-  root: document.body,
-  client: (x?: any, ..._: any) => {
-    if (x === undefined) return new Promise(r => r({}));
-    if (x === "init") return new Promise(r => r(E.right([])));
-    if (x === "view") return new Promise(r => r(E.right([])));
-    if (x === "update") return new Promise(r => r(E.right([])));
-  },
-  listen: (x?: any, ...y: any) => new Promise(r => {
-    console.log(x, ...y);
-    if (x === "msg" && typeof y === "function" && app === null) {
-      app = y;
-      return r({});
-    }
-    if (x === "msg" && typeof y === "object") {
-      // @ts-ignore
-      app(y);
-    }
-    return r({});
-  }),
-  emit: (...x: any) => {
-    console.log(x);
-    return new Promise(r => r({}));
-  },
-  getPluginPaths: new Promise(r => r([
-    "./plugins/ide-view.js",
-    "./plugins/ide-framework.js",
-    "./plugins/counter.js",
-  ])),
-});
-main();
+document.addEventListener("DOMContentLoaded", () => {
+  App({
+    renderHtml,
+    parseHtml,
+    client: (x, args) => {
+      if (x === "init") return new Promise(r => r(E.right([])));
+      if (x === "view") return new Promise(r => r(E.right([])));
+      if (x === "update") return new Promise(r => r(E.right([])));
+      console.error("No client for: ", x, args);
+      return new Promise(r => r(E.right([])));
+    },
+    listen: (x, handler) => {
+      listenHandler.set(x, handler);
+      return new Promise(r => r(null));
+    },
+    emit: (evt, payload) => {
+      const handler = listenHandler.get(evt);
+      if (handler !== undefined) {
+        handler({ payload });
+      }
+      return new Promise(r => r());
+    },
+    getPluginPaths: new Promise(r => r([
+      "./plugins/counter.js",
+    ])),
+    pluginInstallers: [jspInstaller, cssInstaller]
+  });
+})
+
