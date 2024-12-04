be46b5362599f92ae799102717811ab4a27c6188
Nils.Fitjar <nfi005@uib.no>
2024-12-04 15:06:12 +0100

    refactor(ide): corrected entry point

--- ide/entry.ts
+++ ide/entry.ts
@@ -1,16 +1,28 @@
-import { main } from "./index.js";
-import { setup } from "./setup.js";
+import { parseHtml, renderHtml } from "../core/app/lib/renderHtml";
+import client from "../core/app/lib/NmideClient";
+import { emit, listen } from "@tauri-apps/api/event";
+import { getPaths } from "../core/app/ide/getPaths";
+import { InstallHtmlPlugin } from "../core/app/ide/htmlInstaller";
+import { jspInstaller } from "../core/app/lib/jspInstaller";
+import { cssInstaller } from "../core/app/lib/cssInstaller";
+import { App } from "../core/app/App";
 
-setup({
-  cleanup: [],
-  pluginAssets: [],
-  root: document.body,
-  client: (..._: any) => { },
-  log: {
-    info: console.log,
-    error: console.error,
-  },
-  listen: (..._: any) => { },
-  emit: (..._: any) => { },
-});
-main();
+document.addEventListener("DOMContentLoaded", () => {
+  App({
+    cleanup: [],
+    pluginAssets: [],
+    renderHtml,
+    parseHtml,
+    root: document.body,
+    client,
+    log: {
+      info: console.log,
+      error: console.error,
+    },
+    listen,
+    emit,
+    getPluginPaths: getPaths(),
+    pluginInstallers: [InstallHtmlPlugin, jspInstaller, cssInstaller],
+  });
+})
+
