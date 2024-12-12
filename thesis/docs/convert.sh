147c07564b8eb1c31cbee134d9b804a7bedcabb4
Neelzee <nilsien2001@gmail.com>
2024-12-12 12:59:17 +0100

    docs(thesis): Added setup for wiki writing

---
+++ thesis/docs/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function create_doc() {
+  local path=$(dirname $1)
+  local fullname=$(basename $1)
+  local name="${fullname%.*}"
+  mkdir -p "nmide.wiki/$path"
+  touch "nmide.wiki/$path/$name.md"
+  pandoc $1 --to Markdown >"nmide.wiki/$path/$name.md"
+}
+
+for file in $(find "./" -type f -name "*.tex"); do
+  create_doc $file
+done
