f8514fa3ab3c4cd90bbdda042e57c309f0a4361d
Neelzee <nilsien2001@gmail.com>
2025-01-03 15:24:20 +0100

    refactor(core): Started on refactor of core

--- core/app/App.ts
+++ core/app/App.ts
@@ -2,23 +2,19 @@ import {
   AppConfig,
   AppOption,
   defaultConfig,
-  ModelOverwrite,
-  THtml,
-  TMsg
 } from "@nmide/js-utils";
 import { InstallPlugins } from "./lib/InstallPlugins";
-import { Init } from "./lib/Init";
 import "@nmide/js-utils";
 import { pipe } from "fp-ts/lib/function";
 import * as M from "fp-ts/Map";
+import * as O from "fp-ts/Option";
 import * as A from "fp-ts/Array";
 import * as S from "fp-ts/string";
-import { View } from "./lib/View";
-import { Update } from "./lib/Update";
-import { setTimeout } from "timers/promises";
+import * as TE from "fp-ts/TaskEither";
+import { Core, ModuleUnknown as Module } from "@nmide/js-utils/lib/Module";
 
 // TODO: Add docs
-export const App = (opts?: AppOption): void => {
+export const App = (core: Core, opts?: AppOption): void => {
 
   if (opts === undefined) {
     opts = defaultConfig;
@@ -31,6 +27,12 @@ export const App = (opts?: AppOption): void => {
   const config: AppConfig = { ...defaultConfig, ...partialConfig };
 
   window.plugins = new Map();
+  const originalSet = window.plugins.set.bind(window.plugins);
+  window.plugins.set = (key: string, val: Module) => {
+    window.moduleCount--;
+    return originalSet(key, val);
+  };
+  window.moduleCount = 0;
   window.cleanup = config.cleanup;
   window.pluginAssets = config.pluginAssets;
   window.renderHtml = config.renderHtml;
@@ -44,75 +46,36 @@ export const App = (opts?: AppOption): void => {
   window.client = config.client;
   window.coalcePluginState = config.coalcePluginState;
 
-  window.listen<TMsg>("msg", ({ payload: msg }) => {
-    const plugins = M.toArray(S.Ord)(window.plugins);
-    const prevState = window.state;
-    Update(msg, plugins, prevState)
-      .then(newState => {
-        window.state = ModelOverwrite(prevState, newState);
-        window.emit("nmide://update").catch(err => window.log.error("emit update: ", err));
-        return window.state;
-      })
-      // HACK: Try-Catch on cleanup, because it is exsposed to other plugins.
-      // But should we care? If a plugin introduces a new clean-up, is this
-      // pure? Maybe. Should look into this. If this is the case, then we should
-      // handle this better, i.e outside `App.ts`, and only keep the _happy_
-      // path here.
-      .then(state => {
-        window.cleanup.forEach(([pln, clean]) => {
-          try {
-            clean();
-          } catch (err) {
-            window.log.error(`Error on Cleanup from plugin: ${pln}, `, err);
-          }
-        });
-        window.cleanup = []
-        return View(plugins, state);
-      })
-      .then(htmls => pipe(
-        htmls,
-        A.map<[string, THtml], [string, Element | undefined]>(
-          ([x, y]) => [x, window.renderHtml(y)]
-        ),
-        A.filter((x): x is [string, Element] => x[1] !== undefined),
-        A.map<[string, Element], [string, (() => void)]>(([x, y]) => [x, () => window.root.removeChild(y)]),
-      ))
-      .then(cleanup => {
-        window.cleanup = window.cleanup.concat(cleanup);
-      })
-      .then(_ => window.emit("nmide://view").catch(err => window.log.error("emit view: ", err)));
-  });
-
   InstallPlugins()
-    // HACK: This timeout is here, because a plugin is installed by adding a
-    // script-tag, and after this document has finished loading, it will be
-    // added to `window.plugins`, currently there is no way to guarantee that
-    // a plugin has finished _installing_. With the current test-plugins, a
-    // timeout of `250 ms`, has a 100% success rate, but if a Plugin is larger
-    // or does something that takes longer than `250 ms`-post loading, it will
-    // result in the plugin not being loaded, and therefore not being loaded
-    // when the `init`-state happens.
-    .then(_ => setTimeout(250))
-    .then(_ => M.toArray(S.Ord)(window.plugins))
-    .then(plugins => Init(plugins))
-    .then(state => {
-      window.state = state;
-      window.emit("nmide://init").catch(err => window.log.error("emit init: ", err));
-      return state;
-    })
-    .then(tmodel => View(M.toArray(S.Ord)(window.plugins), tmodel))
-    .then(htmls => pipe(
-      htmls,
-      A.map<[string, THtml], [string, Element | undefined]>(
-        ([x, y]) => [x, window.renderHtml(y)]
-      ),
-      A.filter((x): x is [string, Element] => x[1] !== undefined),
-      A.map<[string, Element], [string, (() => void)]>(([x, y]) => [x, () => window.root.removeChild(y)]),
-    ))
-    .then(cleanup => {
-      window.cleanup = window.cleanup.concat(cleanup);
-    })
-    .then(_ => {
-      window.emit("nmide://view").catch(err => window.log.error("emit view: ", err));
-    });
+    .then(() => M.toArray(S.Ord)(window.plugins))
+    .then(modules => pipe(
+      modules,
+      A.map(([moduleName, module]) => pipe(
+        TE.tryCatch<Error, unknown>(
+          // HACK: Modules should be verified after installation
+          () => {
+            const p = module.init(core);
+            if (p instanceof Promise) {
+              return p;
+            } else {
+              return new Promise(
+                (_, reject) => reject(
+                  `Module: ${moduleName}, does not expose a Promise`
+                  + ", and is therefore invalid"
+                )
+              );
+            }
+          },
+          err => new Error(
+            `Module: ${moduleName} threw Error on init: ${JSON.stringify(err)}`
+          ),
+        ),
+        TE.match(
+          err => err,
+          u => u,
+        ),
+        el => el,
+      )),
+      el => el,
+    ));
 };
