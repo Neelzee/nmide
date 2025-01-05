22e4dc52d318119a42f7d8e884c18992b931d732
Neelzee <nilsien2001@gmail.com>
2025-01-05 14:22:01 +0100

    chore(core): Updated plugins

--- core/plugins/trivial.js
+++ core/plugins/trivial.js
@@ -1,11 +1,8 @@
 window.plugins.set("trivial", {
   init: (_) => {
     return {
-      ui: {},
       uiModifications: [],
-      state: [],
       stateModifications: [],
-      events: [],
       eventModifications: [],
     };
   },
