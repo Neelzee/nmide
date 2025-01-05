d17a393707ea02eb8a0902b44bf2e0121dabd9ce
Neelzee <nilsien2001@gmail.com>
2025-01-05 14:18:45 +0100

    feat(core): Added runtime
    Core modifications are now handled.
    Still missing something to render the ui.

--- core/app/App.ts
+++ core/app/App.ts
@@ -2,26 +2,22 @@ import {
   AppConfig,
   AppOption,
   defaultConfig,
-  tObj,
-  TValue,
 } from "@nmide/js-utils";
 import { InstallPlugins } from "./lib/InstallPlugins";
 import "@nmide/js-utils";
 import { pipe } from "fp-ts/lib/function";
 import * as M from "fp-ts/Map";
-import { Monoid } from "fp-ts/Monoid";
-import { foldMap } from "fp-ts/ReadonlyArray";
+import { reduce, map } from "fp-ts/ReadonlyArray";
 import * as A from "fp-ts/Array";
 import * as S from "fp-ts/string";
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
 import * as T from "fp-ts/Task";
 import { ModuleUnknown as Module } from "@nmide/js-utils/lib/Module";
-import { Core } from "@nmide/js-utils/lib/Core";
-import { DCore } from "@nmide/js-utils/lib/Decoder";
+import { Core, CoreModification, EventHandler, } from "@nmide/js-utils/lib/Core";
+import { DCoreModification } from "@nmide/js-utils/lib/Decoder";
 import { evalTree } from "@nmide/js-utils/lib/evalTree";
-import { toNode, Node } from "@nmide/js-utils/lib/tree";
-import { Errors } from "io-ts";
+import { runtime } from "./runtime";
 
 // TODO: Add docs
 export const App = (core: Core, opts?: AppOption): void => {
@@ -36,6 +32,7 @@ export const App = (core: Core, opts?: AppOption): void => {
 
   const config: AppConfig = { ...defaultConfig, ...partialConfig };
 
+  window.core = core;
   window.plugins = new Map();
   const originalSet = window.plugins.set.bind(window.plugins);
   window.plugins.set = (key: string, val: Module) => {
@@ -44,14 +41,11 @@ export const App = (core: Core, opts?: AppOption): void => {
   };
   window.moduleCount = 0;
   window.pluginAssets = config.pluginAssets;
-  window.renderHtml = config.renderHtml;
-  window.parseHtml = config.parseHtml;
   window.root = config.root;
   window.listen = config.listen;
   window.emit = config.emit;
   window.log = config.log;
-  window.getPluginPaths = config.getPluginPaths;
-  window.pluginInstallers = config.pluginInstallers;
+  window.getPluginPaths = config.getPluginPaths; window.pluginInstallers = config.pluginInstallers;
   window.client = config.client;
 
   InstallPlugins()
@@ -59,32 +53,27 @@ export const App = (core: Core, opts?: AppOption): void => {
     .then(modules => pipe(
       modules,
       A.map(([moduleName, module]) => pipe(
-        TE.tryCatch<Error, unknown>(
-          // HACK: Modules should be verified after installation. Or maybe its
-          // okay to this here, I dont really know.
-          () => {
-            if (typeof module.init === "function") {
-              return module.init(core);
-            } else {
-              // TODO: Is there a better way to verify a third-party function?
-              return new Promise(
-                (_, reject) => reject(
-                  `Module: ${moduleName}, does not expose a function`
-                  + ", and is therefore invalid"
-                )
-              );
-            }
-          },
+        TE.tryCatch<Error, [string, unknown]>(
+          async () => typeof module.init === "function"
+            ? [moduleName, await module.init(core)]
+            : new Promise(
+              (_, reject) => reject(
+                `Module: ${moduleName}, does not expose a function`
+                + ", and is therefore invalid"
+              )
+            ),
           err => new Error(
             `Module: ${moduleName} threw Error on init: ${JSON.stringify(err)}`
           ),
         ),
-        TE.match<Error, E.Either<Error, Core>, unknown>(
+        // NOTE: Any output from a Module is unknown, so this step is just to
+        // verify that any given module returns the expected argument
+        TE.match<Error, E.Either<Error, CoreModification>, [string, unknown]>(
           E.left,
           u => pipe(
             u,
-            DCore.decode,
-            E.mapLeft<Errors, Error>(
+            DCoreModification.decode,
+            E.mapLeft(
               err => new Error(`Error on decode: ${JSON.stringify(err)}`)
             ),
           )
@@ -93,74 +82,58 @@ export const App = (core: Core, opts?: AppOption): void => {
       T.sequenceArray,
       T.map(xs => pipe(
         xs,
-        foldMap(CoreMonoid(core))(E.getOrElse(err => {
+        map(E.getOrElse<Error, CoreModification>(err => {
           window.log.error("Error on core folding, post installation:", err);
-          return core;
-        }))
+          return {
+            uiModifications: [],
+            stateModifications: [],
+            eventModifications: [],
+            newEventHandlers: [],
+          };
+        })),
+        reduce(core, coreEvaluation)
       )),
-      task => task(),
-    )) // TODO: Pass the core to the runtime handling event triggering
-    .then(core => core);
+      runtime,
+    ))
 };
 
-const CoreMonoid = (empty: Core): Monoid<Core> => {
+export const coreEvaluation = (
+  core: Core,
+  coreModifications: CoreModification
+): Core => {
   return {
-    empty,
-    concat: CoreSemiGroup,
-  }
-};
-
-// TODO: This seems overkill/stupid/slow, but if it is _just_ milliseconds slow,
-// then it is okay
-const CoreSemiGroup = (a: Core, b: Core): Core => {
-  return {
-    ui: evalTree(
-      { root: toNode(a.ui) },
-      A.concat(a.uiModifications)(b.uiModifications)
-    ).root,
-    uiModifications: [],
+    ...core,
+    ui: pipe(
+      evalTree({ root: core.ui }, coreModifications.uiModifications),
+      tree => tree.root
+    ),
     state: pipe(
-      evalTree(
-        {
-          root: {
-            id: "root",
-            kids: pipe(
-              a.state,
-              A.map(([field, value]) => {
-                return { id: field, kids: [] as Node<TValue>[], ...value };
-              })
-            ),
-            ...tObj([])
-          }
-        },
-        A.concat(a.stateModifications)(b.stateModifications)
-      ),
-      tree => tree.root,
-      node => node.kids,
-      A.map(({ id, kids: _, ...value }) => [id, value])
+      evalTree({ root: core.state }, coreModifications.stateModifications),
+      tree => tree.root
     ),
-    stateModifications: [],
     events: pipe(
-      evalTree(
-        {
-          root: {
-            id: "root",
-            kids: pipe(
-              a.events,
-              A.map(evt => {
-                return toNode(evt);
-              })
-            ),
-            event: "",
-            module: "",
-          }
-        },
-        A.concat(a.eventModifications)(b.eventModifications)
-      ),
-      tree => tree.root,
-      node => node.kids,
-      A.map(({ id: _, kids: __, ...evt }) => evt)
+      evalTree({ root: core.events }, coreModifications.eventModifications),
+      tree => tree.root
     ),
-    eventModifications: []
+    eventHandlers: ((): Map<string, EventHandler[]> => {
+      const map = core.eventHandlers;
+      return pipe(
+        coreModifications.newEventHandlers,
+        // NOTE: Closest I know how to verify that the given handler is valid.
+        // Atleast during runtime.
+        A.filter(([_, { handler }]) => handler.length >= 1),
+        A.map(([e, h]) => {
+          let handlers = map.get(e);
+          if (handlers === undefined) {
+            handlers = [];
+          }
+          // HACK: Is this true? Don't know, but it needs to be true to fit
+          // with the typechecking
+          handlers.push(h as EventHandler)
+          map.set(e, handlers);
+        }),
+        _ => map
+      );
+    })(),
   };
 }
--- core/app/entry.ts
+++ core/app/entry.ts
@@ -5,18 +5,20 @@ import { InstallHtmlPlugin } from "./ide/htmlInstaller";
 import { jspInstaller } from "./lib/jspInstaller";
 import { cssInstaller } from "./lib/cssInstaller";
 import { App } from "./App";
-import { emptyHtml } from "@nmide/js-utils";
+import { emptyHtml, tObj } from "@nmide/js-utils";
+import { toNode } from "@nmide/js-utils/lib/tree";
 
 // TODO: Add docs
 document.addEventListener("DOMContentLoaded", () => {
   App(
     {
-      ui: emptyHtml(),
-      uiModifications: [],
-      state: [],
-      stateModifications: [],
-      events: [],
-      eventModifications: []
+      ui: toNode(emptyHtml()),
+      state: { id: "root", kids: [], ...tObj([]), },
+      events: { id: "root", kids: [], event: "", module: "", },
+      eventThrower: evt => {
+        emit(evt.event, evt);
+      },
+      eventHandlers: new Map(),
     },
     {
       pluginAssets: [],
