507a4f6a62bc16cb73e0c93cd4ad6a0527b18fd3
Neelzee <nilsien2001@gmail.com>
2025-03-14 09:43:06 +0100

    feat: Created flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Haskell Project Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+        project = haskellPackages.callCabal2nix "nmide" ./. { };
+      in
+      {
+        packages.default = project;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # TAURI START
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+            git-cliff
+            xdotool
+            # TAURI END
+            # HASKELL START
+            zlib
+            nodejs
+            electron
+            patchelf
+            gtk2
+            glib
+            cairo
+            pango
+            atk
+            gdk-pixbuf
+            harfbuzz
+            # HASKELL END
+
+            haskellPackages.ghc
+            haskellPackages.cabal-install
+            haskellPackages.haskell-language-server
+            haskellPackages.threepenny-gui
+          ];
+
+        };
+      }
+    );
+}
