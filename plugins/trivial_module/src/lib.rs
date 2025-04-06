d7ebb250714b67c17407e2b1bb6b3cab264c8fb4
Neelzee <nilsien2001@gmail.com>
2025-04-06 19:04:53 +0200

    fix(trivial_module): Added state change on event

--- plugins/trivial_module/src/lib.rs
+++ plugins/trivial_module/src/lib.rs
@@ -1,6 +1,9 @@
 use core_std_lib::{
+    attrs::Attr,
     core::{Core, CoreModification},
     event::Event,
+    html::{Html, UIInstructionBuilder},
+    state::{StateInstructionBuilder, Value},
 };
 
 pub struct ModuleBuilder;
@@ -22,10 +25,40 @@ where
     }
 
     fn init(&self, _core: &T) -> CoreModification {
-        CoreModification::default()
+        let state = StateInstructionBuilder::default().add("counter".to_string(), Value::Int(0));
+        let ui = UIInstructionBuilder::default().add_node(
+            Html::Div()
+                .adopt(
+                    Html::Button()
+                        .text("Click")
+                        .add_attr(Attr::OnClick(Event::new(
+                            "counter".to_string(),
+                            "trivial_module".to_string(),
+                            Some(Value::Int(1)),
+                        )))
+                        .add_attr(Attr::Id("ButtonID".to_string())),
+                )
+                .add_attr(Attr::Id("DivId".to_string())),
+            None,
+            None,
+        );
+        CoreModification::default().set_ui(ui).set_state(state)
     }
 
-    fn handler(&self, _event: &Event, _core: &T) -> CoreModification {
-        CoreModification::default()
+    fn handler(&self, event: &Event, _core: &T) -> CoreModification {
+        match (event.event_name(), event.args()) {
+            ("counter", Some(v)) => {
+                let state = StateInstructionBuilder::default().modify(
+                    "counter".to_string(),
+                    v.clone(),
+                    |l, r| match (l, r) {
+                        (Value::Int(i), Value::Int(j)) => Value::Int(i + j),
+                        _ => Value::Int(0),
+                    },
+                );
+                CoreModification::default().set_state(state)
+            }
+            _ => CoreModification::default(),
+        }
     }
 }
