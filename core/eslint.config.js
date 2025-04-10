7fd67f31c7beb0d59a35c30675a9b140747b77d7
Neelzee <nilsien2001@gmail.com>
2025-04-10 07:52:20 +0200

    fix(core): Corrected eslint-prettier

--- core/eslint.config.js
+++ core/eslint.config.js
@@ -1,10 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
+    ignores: [
+      "dist",
+      "coverage",
+      "node_modules",
+      "src-tauri",
+      "modules",
+      "*.lock",
+      "*.nix",
+    ],
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
@@ -21,4 +31,5 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
