bb4084d86957f56f1eb90d4b8165eaacf77a375c
Neelzee <nilsien2001@gmail.com>
2025-04-11 14:21:18 +0200

    fix(counter-plugin): corrected types

--- modules/counter-plugin/index.ts
+++ modules/counter-plugin/index.ts
@@ -1,14 +1,12 @@
 import { empty_core_modification, build_module, Core, CoreModification, Event } from "core_modification";
 
 const module = "CounterPlugin";
-console.log(empty_core_modification());
-
-const init = (core: Core): Promise<CoreModification> => {
-  throw new Error();
-};
-
-const handler = (event: Event, core: Core): Promise<CoreModification> => {
-  throw new Error();
-};
-
-const mod = build_module(module, init, handler);
+// @ts-ignore
+window.modules.set(
+  module,
+  {
+    name: module,
+    init: async (_: Core) => empty_core_modification(),
+    handler: async (_: Event, __: Core) => empty_core_modification(),
+  }
+);
