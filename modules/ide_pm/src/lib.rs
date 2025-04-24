efca6f51eade08d7e456260e24777ca65c648d67
Neelzee <nilsien2001@gmail.com>
2025-04-24 10:31:59 +0200

    chore(ide_pm): cargo fmt

--- modules/ide_pm/src/lib.rs
+++ modules/ide_pm/src/lib.rs
@@ -1,11 +1,11 @@
-use std::io::Write;
-use std::collections::HashMap;
-use std::fs::{File, OpenOptions};
-use std::path::Path;
 use core_module_lib::Module;
 use core_std_lib::core::{Core, CoreModification};
 use core_std_lib::event::Event;
 use core_std_lib::state::{StateInstructionBuilder, Value};
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
 
 pub struct ModuleBuilder;
 
@@ -30,38 +30,45 @@ impl Module for ProjectManagerModule {
         core.add_handler(
             Some("nmide://exit".to_string()),
             Some("nmide".to_string()),
-            MODULE_NAME.to_string()
-        ).await;
+            MODULE_NAME.to_string(),
+        )
+        .await;
         let mods = CoreModification::default().set_state(
-            StateInstructionBuilder::default()
-                .add(STATE_FIELD, Value::Obj(HashMap::new()))
+            StateInstructionBuilder::default().add(STATE_FIELD, Value::Obj(HashMap::new())),
         );
-        core.get_sender().await.send(mods).await.expect("Channel should be opened");
+        core.get_sender()
+            .await
+            .send(mods)
+            .await
+            .expect("Channel should be opened");
     }
 
     async fn handler(&self, event: Event, core: Box<dyn Core>) {
         match event.event_name() {
             "nmide://exit" => {
-                let obj = core.state().await
+                let obj = core
+                    .state()
+                    .await
                     .get(STATE_FIELD)
                     .and_then(|v| v.clone().obj())
                     .unwrap_or(HashMap::new());
-                let pth =
-                    &obj.get("project_path")
-                        .and_then(|v| v.clone().str())
-                        .unwrap_or("./.nmide.json".to_string());
+                let pth = &obj
+                    .get("project_path")
+                    .and_then(|v| v.clone().str())
+                    .unwrap_or("./.nmide.json".to_string());
                 let project_path = Path::new(pth);
                 if !project_path.exists() {
                     let _ = File::create(project_path).unwrap();
                 }
                 let mut file = OpenOptions::new().append(true).open(project_path).unwrap();
-                let cache = obj.get("cache")
+                let cache = obj
+                    .get("cache")
                     .and_then(|v| v.clone().obj())
                     .unwrap_or(HashMap::new());
 
                 writeln!(file, "{}", serde_json::to_string_pretty(&cache).unwrap()).unwrap();
-            },
-            _ => ()
+            }
+            _ => (),
         }
     }
 }
