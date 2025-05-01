38b9179dd2f197e698854a3fff30f5f8d5198abe
Neelzee <nilsien2001@gmail.com>
2025-05-01 15:04:58 +0200

    feat(module-tester): Started on js system

--- module-tester/build.ts
+++ module-tester/build.ts
@@ -1,35 +1,59 @@
+import { exec } from "child_process";
 import * as fs from "fs";
-
-const modules_regx = /.*\s=\s{.*}/gm;
-const table_regx = /{.*}/gm
-const path_regx = /path\s=\s".*"/gm
-const kind_regx = /kind\s=\s".*"/gm
+import toml from "toml";
+import util from "util";
+import path from "node:path";
 
 const data = fs.readFileSync("./Modules.toml", "utf-8");
 
 type Module = {
   name: string
-  path?: string,
-  kind?: string,
+  path: string,
+  kind: string,
+  enabled: boolean,
+  "package-manager"?: string,
+};
+
+type TomlData = {
+  modules: { [key in string]?: Omit<Partial<Module>, "name"> }
+};
+
+let tomlData: TomlData | undefined = undefined;
+try {
+  tomlData = toml.parse(data);
+} catch (e) {
+  console.error(e);
 }
 
-const modules: Module[] = modules_regx[Symbol.match](data)?.map(s => {
-  const name = s.split("=")[0]?.trim();
-  const table = table_regx[Symbol.match](s)?.at(0)!;
-  const path = path_regx[Symbol.match](table)?.at(0);
-  const kind = kind_regx[Symbol.match](table)?.at(0);
-  return { name: name === undefined ? "" : name,
-           path: path === undefined
-             ? ""
-             : path.split("=")[1]?.trim()
-               .replace("\"", "")
-               .replace("\"", ""),
-           kind: kind === undefined
-             ? "rs"
-             : kind.split("=")[1]?.trim()
-                .replace("\"", "")
-                .replace("\"", ""),
-         };
-})!;
+if (tomlData === undefined) {
+  throw new Error("tomlData is undefined");
+};
 
-console.log(modules);
+const parseData = ({ modules: data }: TomlData): Module[] =>
+  Object.keys(data)
+    .map((k): Partial<Omit<Module, "name">> & { name: string } => {
+      return { ...data[k], name: k };
+    })
+    .filter(m => m?.enabled === undefined ? true : m.enabled)
+    .filter(m => m?.kind !== undefined)
+    .filter(m => m!.kind !== "rs")
+    .filter(m => m!.kind !== "css")
+    .map((m): Module => {
+      const { name, path, kind } = m;
+      return {
+        name,
+        path: path === undefined
+          ? `../modules/${name}`
+          : path,
+        kind: kind!,
+        enabled: true,
+        "package-manager": m["package-manager"]
+      };
+    });
+
+
+export const install = () => {
+  parseData(tomlData).forEach(m => {
+    require(m.path);
+  });
+};
