9e0b7272bbd30497154f37f832e953bb89c0d37e
Neelzee <nilsien2001@gmail.com>
2025-05-08 14:33:14 +0200

    fix(js-utils): Corrected attr type

--- libs/js-utils/lib/AttrUtils.ts
+++ libs/js-utils/lib/AttrUtils.ts
@@ -1,13 +1,13 @@
 import { Event } from "./Event.ts";
 
 export const id = (id: string): { id: string } => {
-  return { id };
+    return { id };
 }
 
-export const cls = (cls: string): { class: string } => {
-  return { class: cls };
+export const cls = (cls: string): { clss: string } => {
+    return { clss: cls };
 }
 
 export const click = (event: Event): { click: Event } => {
-  return { click: event };
+    return { click: event };
 }
