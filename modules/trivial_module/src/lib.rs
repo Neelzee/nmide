03a5b930a95132fd325ea7fb1a13028d7d405378
Neelzee <nilsien2001@gmail.com>
2025-05-08 14:37:26 +0200

    fix(trivial_module): Made use of post-init event

--- modules/trivial_module/src/lib.rs
+++ modules/trivial_module/src/lib.rs
@@ -26,33 +26,19 @@ impl core_module_lib::Module for Module {
 
     async fn init(&self, core: Box<dyn Core>) {
         let state = StateInstructionBuilder::default().add("counter".to_string(), Value::Int(0));
-        let ui = UIInstructionBuilder::default().add_node(
-            Html::Div()
-                .adopt(
-                    Html::Button()
-                        .set_text("Click")
-                        .add_attr(Attr::Click(Event::new(
-                            "counter".to_string(),
-                            "trivial_module".to_string(),
-                            Some(Value::Int(1)),
-                        )))
-                        .add_attr(Attr::Id("ButtonID".to_string())),
-                )
-                .adopt(
-                    Html::P()
-                        .set_text("Count: 0")
-                        .add_attr(Attr::Id("PID".to_string())),
-                )
-                .add_attr(Attr::Id("DivId".to_string())),
-            Option::<String>::None,
-        );
         core.add_handler(
             Some("counter".to_string()),
             None,
             "trivial_module".to_string(),
         )
         .await;
-        let mods = CoreModification::default().set_ui(ui).set_state(state);
+        core.add_handler(
+            Some("post-init".to_string()),
+            None,
+            "trivial_module".to_string(),
+        )
+        .await;
+        let mods = CoreModification::default().set_state(state);
         core.get_sender()
             .await
             .send(mods)
@@ -62,38 +48,59 @@ impl core_module_lib::Module for Module {
 
     async fn handler(&self, event: Event, core: Box<dyn Core>) {
         let sender = core.get_sender().await;
-        println!("{:?}", event.args());
         match (event.event_name(), event.args()) {
+            ("post-init", _) => {
+                let ui = UIInstructionBuilder::default().add_node(
+                    Html::Div()
+                        .adopt(
+                            Html::Button()
+                                .set_text("Click")
+                                .add_attr(Attr::Click(Event::new(
+                                    "counter".to_string(),
+                                    "trivial_module".to_string(),
+                                    Some(Value::Int(1)),
+                                )))
+                                .add_attr(Attr::Id("ButtonID".to_string())),
+                        )
+                        .adopt(
+                            Html::P()
+                                .set_text("Count: 0")
+                                .add_attr(Attr::Id("PID".to_string())),
+                        )
+                        .add_attr(Attr::Id("DivId".to_string())),
+                    Some("content"),
+                );
+                sender
+                    .send(CoreModification::default().set_ui(ui))
+                    .await
+                    .expect("Channel should be opened");
+            }
             ("counter", Some(v))
                 if v.is_int() || v.obj().is_some_and(|o| o.contains_key("eventArgs")) =>
             {
-                let state = StateInstructionBuilder::default().set(
-                    "counter".to_string(),
-                    if v.is_int() {
-                        v.clone()
-                    } else {
-                        v.obj().unwrap().get("eventArgs").unwrap().clone()
-                    },
-                );
-                let ui = if let Some(v) = core.state().await.get("counter") {
-                    UIInstructionBuilder::default().set_text(
-                        Some("PID"),
-                        format!(
-                            "Count: {}",
-                            if v.is_int() {
-                                v.clone().int().unwrap()
-                            } else {
-                                v.clone()
-                                    .obj()
-                                    .and_then(|o| o.get("eventArgs").cloned())
-                                    .and_then(|val| val.clone().int())
-                                    .unwrap_or(0)
-                            }
-                        ),
-                    )
+                let add = if v.is_int() {
+                    v.clone().int().unwrap()
                 } else {
-                    UIInstructionBuilder::default()
+                    v.obj()
+                        .unwrap()
+                        .get("eventArgs")
+                        .unwrap()
+                        .clone()
+                        .int()
+                        .unwrap()
                 };
+                let new_count = core
+                    .state()
+                    .await
+                    .get("counter")
+                    .cloned()
+                    .unwrap_or(Value::Int(0))
+                    .map(|i: i32| i + add)
+                    .unwrap();
+                let state = StateInstructionBuilder::default()
+                    .set("counter".to_string(), new_count.clone());
+                let ui = UIInstructionBuilder::default()
+                    .set_text(Some("PID"), format!("Count: {}", new_count.int().unwrap()));
                 let mods = CoreModification::default().set_state(state).set_ui(ui);
                 sender.send(mods).await.expect("Channel should be opened");
             }
