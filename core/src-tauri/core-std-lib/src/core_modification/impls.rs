13dd43b9d615698bc82cbc352c8aafe2635e969e
Neelzee <nilsien2001@gmail.com>
2025-05-08 18:47:54 +0200

    feat(core-std-lib): Added CoreModification optimalization

--- core/src-tauri/core-std-lib/src/core_modification/impls.rs
+++ core/src-tauri/core-std-lib/src/core_modification/impls.rs
@@ -79,4 +79,65 @@ impl CoreModification {
     pub fn get_html_instr(&self) -> Instruction<Html> {
         self.ui.0.clone()
     }
+
+    /// Optimizes the modification
+    ///
+    /// Since an `Instruction<T>` is a group, we can reduce any `Instruction<T>`
+    /// where T implements `Eq`, by removing reduntant instructions. A
+    /// redundant instruction is an instruction, that when `combine`-ed results
+    /// in a `NoOp`, one that results in no change, or a `NoOp`.
+    ///
+    /// A small example:
+    /// ```rust
+    /// use core_std_lib::instruction::inst::Instruction;
+    ///
+    /// let add = Instruction::Add("foo".to_string(), 1);
+    /// let rem = Instruction::Rem("foo".to_string(), 1);
+    /// let combined = add.combine(rem);
+    /// assert_eq!(combined, Instruction::NoOp);
+    /// ```
+    ///
+    /// This combination does not recursivly check for `NoOp`s:
+    ///
+    /// ```rust
+    /// use core_std_lib::instruction::inst::Instruction;
+    ///
+    /// let add = Instruction::Add("foo".to_string(), 1);
+    /// let add_2 = Instruction::Add("foobar".to_string(), 1);
+    /// let rem = Instruction::Rem("foo".to_string(), 1);
+    /// let combined = add.combine(add_2).combine(rem);
+    /// assert_ne!(combined, Instruction::NoOp);
+    /// ```
+    ///
+    /// This is "fixed" by using the `Instruction::optimize`, which recursivly
+    /// optimizes for this:
+    ///
+    /// ```rust
+    /// use core_std_lib::instruction::inst::Instruction;
+    ///
+    /// let add = Instruction::Add("foo".to_string(), 1);
+    /// let add_2 = Instruction::Add("foobar".to_string(), 1);
+    /// let rem = Instruction::Rem("foo".to_string(), 1);
+    /// let combined = Instruction::optimize(vec![add.combine(add_2).combine(rem)]);
+    /// assert_eq!(combined, Instruction::Add("foobar".to_string(), 1));
+    /// ```
+    ///
+    /// We can also do further optimalization, since we don't on only work
+    /// on `T`, we can also work on different `T`, the way we have structured
+    /// the UI, makes it so that there are three instructions for changing the
+    /// UI, and these three instructions habe dependencies. For example, if we
+    /// `Rem`-ove an Html node, that another `Add`-instruction were going to
+    /// add an `Attr` to, we can optimize away this `Add`-instruction, as it is
+    /// effectivly an `NoOp`.
+    ///
+    pub fn optimize(self) -> Self {
+        Self {
+            state: Instruction::optimize(vec![self.state]),
+            ui: (
+                Instruction::optimize(vec![self.ui.0]),
+                Instruction::optimize(vec![self.ui.1]),
+                Instruction::optimize(vec![self.ui.2]),
+            ),
+        }
+    }
 }
