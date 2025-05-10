1cd2ede81910f0a9530c6165333a965b5f3fe816
Neelzee <nilsien2001@gmail.com>
2025-05-10 11:17:50 +0200

    feat!(core-std-lib): changed modifications sending
    Instead of a Module getting a Sender, it instead just invokes the Core
    with the CoreModifications to be sent in the channel, leaving the Error
    handling to be done on the Core side.

--- core/src-tauri/core-std-lib/src/core.rs
+++ core/src-tauri/core-std-lib/src/core.rs
@@ -1,7 +1,6 @@
 use crate::core_modification::CoreModification;
 use crate::{event::Event, html::Html, state::State};
 use async_trait::async_trait;
-use tokio::sync::mpsc::Sender;
 
 #[async_trait]
 pub trait Core: Send + Sync {
@@ -9,5 +8,5 @@ pub trait Core: Send + Sync {
     async fn ui(&self) -> Html;
     async fn throw_event(&self, event: Event);
     async fn add_handler(&self, event_name: String, handler_name: String);
-    async fn get_sender(&self) -> Sender<CoreModification>;
+    async fn send_modification(&self, modification: CoreModification);
 }
