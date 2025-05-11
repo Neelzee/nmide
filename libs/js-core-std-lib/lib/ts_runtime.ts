a2a96158275bc73df85f74ea1648e2bcbdc258f6
Neelzee <nilsien2001@gmail.com>
2025-05-11 18:06:35 +0200

    fix(js-core-std-lib): corrected core building

--- libs/js-core-std-lib/lib/ts_runtime.ts
+++ libs/js-core-std-lib/lib/ts_runtime.ts
@@ -6,6 +6,7 @@ import {
   State,
   HtmlBuilder,
   Html,
+  getEventName,
 } from "@nmide/js-utils";
 import "./app";
 import client from "@nmide/js-client";
@@ -15,14 +16,13 @@ import { emit } from "@tauri-apps/api/event";
 
 const registerHandler = async (
   module: string,
-  event_name?: string,
+  event_name: string,
 ) => {
-  if (event_name !== undefined) {
-    let list = window.__nmideConfig__.handlerRegister.event.get(event_name)
-    list = list === undefined ? [] : list;
-    list.push(module);
-    window.__nmideConfig__.handlerRegister.event.set(event_name, list);
-  }
+  window.__nmideConfig__.log.info(`Module: ${module} -> ${event_name}`);
+  let list = window.__nmideConfig__.handlerRegister.event.get(event_name)
+  list = list === undefined ? [] : list;
+  list.push(module);
+  window.__nmideConfig__.handlerRegister.event.set(event_name, list);
 };
 
 const eventThrower = async (event: Event) => {
@@ -55,23 +55,19 @@ const mkCore = async (): Promise<Core> => {
 }
 
 const tsHandler = async (evt: Event) => {
-  if (typeof evt === "object" && "event" in evt) {
-    const { event, args } = evt.event;
-    const core: Core = await mkCore();
+  const core: Core = await mkCore();
 
-    const event_modules = window.__nmideConfig__.handlerRegister.event.get(event);
-    const modules = event_modules === undefined ? [] : event_modules;
-    // TODO: Add proper validation/handling
-    const modifications: CoreModification[] = await Promise.all(
-      modules
-        .map(m => window.__nmideConfig__.modules.get(m))
-        .filter(m => m !== undefined)
-        .map(m => m.handler({ event: { event, args } }, core))
-    );
+  const event_modules = window.__nmideConfig__.handlerRegister.event.get(getEventName(evt));
+  const modules = event_modules === undefined ? [] : event_modules;
+  // TODO: Add proper validation/handling
+  const modifications: CoreModification[] = await Promise.all(
+    modules
+      .map(m => window.__nmideConfig__.modules.get(m))
+      .filter(m => m !== undefined)
+      .map(m => m.handler(evt, core))
+  );
 
-    return modifications;
-  }
-  return [];
+  return modifications;
 }
 
 const tsInit = async () => {
