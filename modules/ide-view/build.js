d3c7a36e1d84b7d64549fa44b6bdcc64ce11e9e4
Neelzee <nilsien2001@gmail.com>
2025-05-15 16:26:18 +0200

    chore: removed deprecated module

--- modules/ide-view/build.js
+++
@@ -1,9 +0,0 @@
-const esbuild = require('esbuild');
-
-esbuild.build({
-  entryPoints: ['./index.ts'],
-  bundle: true,
-  outfile: "../../core/plugins/ide-view.js",
-  minify: true,
-}).catch(() => process.exit(1));
-
