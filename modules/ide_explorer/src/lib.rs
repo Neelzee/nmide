d12b42527a141752518bc503c9d185fae8d843ca
Neelzee <nilsien2001@gmail.com>
2025-05-16 12:24:04 +0200

    feat(ide_explorer): ide can now explore

--- modules/ide_explorer/src/lib.rs
+++ modules/ide_explorer/src/lib.rs
@@ -5,8 +5,16 @@ use core_std_lib::{
     core_modification::CoreModification,
     event::Event,
     html::{Html, UIInstructionBuilder},
-    state::Value,
+    state::{StateInstructionBuilder, Value},
 };
+
+pub struct ModuleBuilder;
+
+impl core_module_lib::ModuleBuilder for ModuleBuilder {
+    fn build(self) -> impl core_module_lib::Module {
+        Module
+    }
+}
 
 pub struct Module;
 
@@ -20,6 +28,8 @@ impl core_module_lib::Module for Module {
 
     async fn init(&self, core: Box<dyn Core>) {
         core.add_handler("open-project".to_string(), MODULE_NAME.to_string())
+            .await;
+        core.add_handler("toggle-folder".to_string(), MODULE_NAME.to_string())
             .await;
     }
 
@@ -45,11 +55,50 @@ impl core_module_lib::Module for Module {
                             Html::Div()
                                 .adopt(html)
                                 .add_attr(Attr::Class("file-explorer".to_string())),
-                            Some("side-bar"),
+                            Some("sidebar"),
                         ),
                     ),
                 )
-                .await
+                .await;
+                core.throw_event(Event::new("open-project-post", None))
+                    .await;
+            }
+            "toggle-folder" => {
+                let args = event.args().and_then(|v| v.obj()).unwrap_or_default();
+                let id = args.get("eventArgs").and_then(|v| v.str());
+                if id.is_none() {
+                    return;
+                }
+                let id = id.unwrap().replace("/", "_");
+
+                let state = core.state().await;
+
+                let collapsed = state.get(&id).and_then(|v| v.bool()).unwrap_or(true);
+
+                let state_builder =
+                    StateInstructionBuilder::default().add(id.clone(), Value::Bool(!collapsed));
+
+                if collapsed {
+                    core.send_modification(
+                        CoreModification::default()
+                            .set_ui(
+                                UIInstructionBuilder::default()
+                                    .rem_attr(Attr::Class("collapsed".to_string()), id),
+                            )
+                            .set_state(state_builder),
+                    )
+                    .await;
+                } else {
+                    core.send_modification(
+                        CoreModification::default()
+                            .set_ui(
+                                UIInstructionBuilder::default()
+                                    .add_attr(id, Attr::Class("collapsed".to_string())),
+                            )
+                            .set_state(state_builder),
+                    )
+                    .await;
+                }
             }
             _ => (),
         }
@@ -63,17 +112,25 @@ fn render(f: Fo) -> Html {
 
             Html::Span()
                 .set_text(file_name)
-                .add_attr(Attr::Id(path))
+                .add_attr(Attr::Id(path.replace("/", "_")))
                 .add_attr(Attr::Class("file".to_string()))
         }
         Fo::Folder(path, fos) => {
             let file_name = path.split("/").last().unwrap_or(&path);
-
+            let mut kids =
+                vec![
+                    Html::Span()
+                        .set_text(file_name)
+                        .add_attr(Attr::Click(Event::new(
+                            "toggle-folder",
+                            Some(Value::Str(path.replace("/", "_"))),
+                        ))),
+                ];
+            kids.append(&mut fos.into_iter().map(|k| render(k)).collect());
             Html::Div()
-                .set_text(file_name)
-                .add_attr(Attr::Id(path))
-                .add_attr(Attr::Class("folder".to_string()))
-                .replace_kids(fos.into_iter().map(|k| render(k)).collect())
+                .add_attr(Attr::Id(path.replace("/", "_")))
+                .add_attr(Attr::Class("folder collapsed".to_string()))
+                .replace_kids(kids)
         }
     }
 }
