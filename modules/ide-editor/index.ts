388bcdcaba75b60bb449a47717aa2e2740255068
Neelzee <nilsien2001@gmail.com>
2025-05-18 21:25:57 +0200

    feat(ide-editor): Corrected editor impl

--- modules/ide-editor/index.ts
+++ modules/ide-editor/index.ts
@@ -1,9 +1,11 @@
 import {
+  click,
+  cls,
   Core,
   CoreModification,
   emptyCm,
   Event, HtmlBuilder,
-  installModule, UiBuilder
+  installModule, isPrimAnd, mkPrimEvent, UiBuilder
 } from "@nmide/js-utils";
 
 const moduleName = "ide-editor";
@@ -14,30 +16,27 @@ installModule(
     init: async (core: Core): Promise<CoreModification> => {
       await core.registerHandler(moduleName, "editor-click")
         .catch(err => console.error(moduleName, err));
-      await core.registerHandler(moduleName, "post-init")
+      await core.registerHandler(moduleName, "open-editor-area")
         .catch(err => console.error(moduleName, err));
       return emptyCm();
     },
-    handler: async (event: Event, _: Core): Promise<CoreModification> => {
-      if (event.event === "editor-click") {
-        console.log("editor: ", event);
-      }
-      if (event.event === "post-init") {
-        return new UiBuilder()
-          .add(
-            new HtmlBuilder()
-              .attrs({ id: "editor-div" })
-              .kids(
-                new HtmlBuilder()
-                  .kind("textArea")
-                  .attrs(
-                    { id: "editor-area" },
-                    { click: { event: "editor-click", module: moduleName, args: null } },
-                  )
-              ),
-            "content"
-          )
-          .build();
+    handler: async (event: Event, core: Core): Promise<CoreModification> => {
+      if (isPrimAnd(event, "open-editor-area")) {
+        await core.eventThrower(mkPrimEvent("add_content",
+          new HtmlBuilder()
+            .attrs(
+              { id: "editor-div" },
+              cls("editor-container")
+            )
+            .kids(
+              new HtmlBuilder()
+                .kind("textArea")
+                .attrs(
+                  { id: "editor-area" },
+                  click(mkPrimEvent("editor-click"))
+                )
+            ).build()
+        ));
       }
       return emptyCm();
     }
