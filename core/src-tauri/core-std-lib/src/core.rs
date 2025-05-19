da613d98011f75521c2d70b2b65bbde378dff66e
Neelzee <nilsien2001@gmail.com>
2025-05-19 13:16:20 +0200

    feat(core-std-lib): Added appdir method to core

--- core/src-tauri/core-std-lib/src/core.rs
+++ core/src-tauri/core-std-lib/src/core.rs
@@ -1,6 +1,7 @@
 use crate::core_modification::CoreModification;
 use crate::{event::Event, html::Html, state::State};
 use async_trait::async_trait;
+use std::path::PathBuf;
 
 #[async_trait]
 pub trait Core: Send + Sync {
@@ -9,4 +10,5 @@ pub trait Core: Send + Sync {
     async fn throw_event(&self, event: Event);
     async fn add_handler(&self, event: String, handler: String);
     async fn send_modification(&self, modification: CoreModification);
+    async fn appdir(&self) -> PathBuf;
 }
