bc5c3dc8c8315cb61074b719d5dc86c19e087aee
Neelzee <nilsien2001@gmail.com>
2025-05-20 18:29:35 +0200

    feat(module_installer): Added github installation

--- modules/module_installer/src/lib.rs
+++ modules/module_installer/src/lib.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 use async_trait::async_trait;
 use core_std_lib::{
     attrs::Attr,
@@ -9,6 +7,10 @@ use core_std_lib::{
     html::{Html, UIInstructionBuilder},
     state::{StateInstructionBuilder, Value},
 };
+use module_installer::ModuleInstaller;
+use std::fs;
+
+pub(crate) mod module_installer;
 
 pub struct ModuleBuilder;
 
@@ -33,38 +35,48 @@ impl core_module_lib::Module for Module {
             .await;
         core.add_handler("toggle-add-module".to_string(), NAME.to_string())
             .await;
-        let ui = Html::Div()
-            .add_attr(Attr::Id("add-module-container".to_string()))
-            .add_attr(Attr::Class("add-module-hide".to_string()))
-            .adopt(
-                Html::Button()
-                    .set_text("x")
-                    .add_attr(Attr::Class("close-btn".to_string()))
-                    .add_attr(Attr::Click(Event::new(
-                        "toggle-add-module".to_string(),
-                        None,
-                    ))),
-            )
-            .adopt(
-                Html::Form()
-                    .add_attr(Attr::Id("add-module-form".to_string()))
-                    .adopt(
-                        Html::Div()
-                            .adopt(Html::Label().set_text("Module path").add_attr(Attr::Custom(
-                                "for".to_string(),
-                                "module_path".to_string(),
-                            )))
-                            .adopt(Html::Input().add_attr(Attr::Custom(
-                                "name".to_string(),
-                                "module_path".to_string(),
-                            ))),
-                    )
-                    .adopt(
-                        Html::Button()
-                            .add_attr(Attr::Click(Event::new("add-module", None)))
-                            .set_text("Install"),
-                    ),
-            );
+        let ui =
+            Html::Div()
+                .add_attr(Attr::Id("add-module-container".to_string()))
+                .add_attr(Attr::Class("add-module-hide".to_string()))
+                .adopt(
+                    Html::Button()
+                        .set_text("x")
+                        .add_attr(Attr::Class("close-btn".to_string()))
+                        .add_attr(Attr::Click(Event::new(
+                            "toggle-add-module".to_string(),
+                            None,
+                        ))),
+                )
+                .adopt(
+                    Html::Form()
+                        .add_attr(Attr::Id("add-module-form".to_string()))
+                        .adopt(
+                            Html::Div()
+                                .adopt(Html::Label().set_text("Module path: ").add_attr(
+                                    Attr::Custom("for".to_string(), "module_path".to_string()),
+                                ))
+                                .adopt(Html::Input().add_attr(Attr::Custom(
+                                    "name".to_string(),
+                                    "module_path".to_string(),
+                                ))),
+                        )
+                        .adopt(
+                            Html::Div()
+                                .adopt(Html::Label().set_text("GitHub url: ").add_attr(
+                                    Attr::Custom("for".to_string(), "module_url".to_string()),
+                                ))
+                                .adopt(Html::Input().add_attr(Attr::Custom(
+                                    "name".to_string(),
+                                    "module_url".to_string(),
+                                ))),
+                        )
+                        .adopt(
+                            Html::Button()
+                                .add_attr(Attr::Click(Event::new("add-module", None)))
+                                .set_text("Install"),
+                        ),
+                );
         core.send_modification(CoreModification::ui(
             UIInstructionBuilder::default().add_node(ui, Option::<String>::None),
         ))
@@ -101,8 +113,18 @@ impl core_module_lib::Module for Module {
             }
             ("add-module", Some(val)) => {
                 let args = val.obj().unwrap().get("form").unwrap().obj().unwrap();
-                let module_path = args.get("module_path").unwrap().str().unwrap();
-                fs::copy(module_path, core.appdir().await).unwrap();
+                let dir = core.appdir().await;
+                if let Some(module_path) = args.get("module_path").unwrap().str() {
+                    fs::copy(module_path, dir).unwrap();
+                } else if let Some(url) = args.get("module_url").and_then(|v| v.str()) {
+                    let installer = ModuleInstaller::new(dir.to_str().unwrap());
+                    match installer.install_from_github(&url).await {
+                        Ok(pth) => {
+                            fs::copy(pth, dir).unwrap();
+                        }
+                        Err(err) => todo!("Handle error: ${err:?}"),
+                    }
+                }
             }
             _ => (),
         }
