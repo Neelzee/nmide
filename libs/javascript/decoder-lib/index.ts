f78c0285a7e67091b023926251dba64894247e44
Neelzee <nilsien2001@gmail.com>
2025-05-25 14:29:50 +0200

    feat(js-decoder-lib): added re-export of error reporter

--- libs/javascript/decoder-lib/index.ts
+++ libs/javascript/decoder-lib/index.ts
@@ -4,3 +4,5 @@ export * from "./lib/value_decoder";
 export * from "./lib/core_modification_decoder";
 export * from "./lib/module_decoder";
 export * from "./lib/instr_decoder";
+import pr from "io-ts-reporters";
+export const prettyReport = pr;
