0ab3e385c1e1f6e18af18b7bf505640f4030565a
Neelzee <nilsien2001@gmail.com>
2025-05-26 07:55:35 +0200

    chore: removed DART from possible names

--- draftName.sh
+++ draftName.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# List of names
-names=("Artemis" "Apollo" "Gemini" "Mercury" "Orion" "Voyager" "Pioneer" "Mariner" "Cassini" "Juno" "Galileo" "Hubble" "Kepler" "Spitzer" "Dragonfly" "DART" "Perseverance" "Curiosity" "Opportunity" "Spirit" "Pathfinder" "Starliner" "Skylab" "Insight" "TESS" "Lucy" "Osiris-Rex")
+names=("Artemis" "Apollo" "Gemini" "Mercury" "Orion" "Voyager" "Pioneer" "Mariner" "Cassini" "Juno" "Galileo" "Hubble" "Kepler" "Spitzer" "Dragonfly" "Perseverance" "Curiosity" "Opportunity" "Spirit" "Pathfinder" "Starliner" "Skylab" "Insight" "TESS" "Lucy" "Osiris-Rex")
 
-# Optional seed passed as the first argument
 if [ -n "$1" ]; then
   seed="$1"
   RANDOM=$seed
 fi
 
-# Get a random index based on the list length
 index=$((RANDOM % ${#names[@]}))
 
-# Output the selected name
 echo "${names[$index]}"
