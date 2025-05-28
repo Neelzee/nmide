e7f5a18118d75f4ff99889212e31c792d9f76ece
Neelzee <nilsien2001@gmail.com>
2025-05-28 09:36:41 +0200

    feat(editor-ar): Added serializing to and from Value

--- modules/editor-ar/src/lib.rs
+++ modules/editor-ar/src/lib.rs
@@ -1,3 +1,5 @@
+use core_std_lib::state::Value;
+
 pub enum Art {
     Token { pos: Position, char: char },
     Line { pos: Position, line: Vec<Art> },
@@ -37,6 +39,39 @@ impl Position {
             self.end_col_pos,
         )
     }
+
+    pub fn from_value(value: Value) -> Option<Self> {
+        match value {
+            Value::Obj(mp) => {
+                let map = mp.to_hm();
+                let start_row_pos = map.get("start_row_pos").cloned().and_then(|a| a.int())?;
+                let end_row_pos = map.get("end_row_pos").cloned().and_then(|a| a.int())?;
+                let start_col_pos = map.get("start_col_pos").cloned().and_then(|a| a.int())?;
+                let end_col_pos = map.get("end_col_pos").cloned().and_then(|a| a.int())?;
+
+                let start_row_pos = start_row_pos.unsigned_abs() as usize;
+                let end_row_pos = end_row_pos.unsigned_abs() as usize;
+                let start_col_pos = start_col_pos.unsigned_abs() as usize;
+                let end_col_pos = end_col_pos.unsigned_abs() as usize;
+
+                Some(Self {
+                    start_row_pos,
+                    end_row_pos,
+                    start_col_pos,
+                    end_col_pos,
+                })
+            }
+            _ => None,
+        }
+    }
+
+    pub fn to_value(self) -> Value {
+        Value::new_obj()
+            .add("start_row_pos", Value::Int(self.start_row_pos as i32))
+            .add("end_row_pos", Value::Int(self.end_row_pos as i32))
+            .add("start_col_pos", Value::Int(self.start_col_pos as i32))
+            .add("end_col_pos", Value::Int(self.end_col_pos as i32))
+    }
 }
 
 impl Art {
@@ -64,6 +99,47 @@ impl Art {
     pub fn get_pos(&self) -> (usize, usize, usize, usize) {
         match self {
             Art::Token { pos, .. } | Art::Line { pos, .. } => pos.dec(),
+        }
+    }
+
+    pub fn from_value(value: Value) -> Option<Self> {
+        match value {
+            Value::Obj(mp) => {
+                let map = mp.to_hm();
+                let pos = Position::from_value(map.get("pos").cloned()?)?;
+
+                if let Some(s) = map.get("char").and_then(|v| v.str()) {
+                    return Some(Self::Token {
+                        pos,
+                        char: s.chars().find(|_| true)?,
+                    });
+                }
+
+                if let Some(s) = map.get("line").and_then(|v| v.list()) {
+                    return Some(Self::Line {
+                        pos,
+                        line: s
+                            .into_iter()
+                            .map(Self::from_value)
+                            .collect::<Option<Vec<_>>>()?,
+                    });
+                }
+
+                None
+            }
+            _ => None,
+        }
+    }
+
+    pub fn to_value(self) -> Value {
+        match self {
+            Art::Token { pos, char } => Value::new_obj()
+                .add("pos", pos.to_value())
+                .add("char", Value::Str(char.to_string())),
+            Art::Line { pos, line } => Value::new_obj().add("pos", pos.to_value()).add(
+                "line",
+                Value::List(line.into_iter().map(Self::to_value).collect()),
+            ),
         }
     }
 }
