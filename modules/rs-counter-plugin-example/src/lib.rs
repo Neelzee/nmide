8a7c9eebea1b6f9d7c9e1233cf62063c9ee96238
Neelzee <nilsien2001@gmail.com>
2025-06-01 22:44:53 +0200

    feat(rs-counter-plugin-example): Added functionality

--- modules/rs-counter-plugin-example/src/lib.rs
+++ modules/rs-counter-plugin-example/src/lib.rs
@@ -1,8 +1,15 @@
-use abi_stable::{export_root_module, prefix_type::PrefixTypeTrait, sabi_extern_fn};
+use abi_stable::{
+    export_root_module,
+    prefix_type::PrefixTypeTrait,
+    rvec, sabi_extern_fn,
+    std_types::{ROption, RString, RVec},
+};
 use async_ffi::{FfiFuture, FutureExt};
 use core_module_lib::rs_module::{ModuleRef, RCore_CTO, RustModule};
+use core_std_lib::{event::Event, state::Value};
 use foreign_std_lib::{
-    core::rs_core_modification::RCoreModification, event::rs_event::REvent, state::rs_state::RValue,
+    attr::rs_attrs::RAttr, core::rs_core_modification::RCoreModification, event::rs_event::REvent,
+    html::rs_html::RHtml, state::rs_state::RValue,
 };
 
 #[export_root_module]
@@ -11,12 +18,50 @@ pub fn get_library() -> ModuleRef {
 }
 
 #[sabi_extern_fn]
-pub fn init(core: RCore_CTO) -> FfiFuture<()> {
-    core.send_modification(RCoreModification::default().add_field("foobar", RValue::new_int(10)))
-        .into_ffi()
+pub fn init(core: RCore_CTO<'static, 'static>) -> FfiFuture<()> {
+    async move {
+        core.send_modification(
+            RCoreModification::default()
+                .add_field("rs-count", RValue::new_int(1))
+                .add_node(
+                    "",
+                    RHtml::Button(
+                        RVec::new(),
+                        rvec![
+                            RAttr::new_click(REvent::from(Event::new(
+                                "rs-count",
+                                Some(Value::Int(1))
+                            ))),
+                            RAttr::new_id("rs-btn")
+                        ],
+                    )
+                    .set_text("Click"),
+                ),
+        )
+        .await;
+        core.add_handler(
+            RString::from("rs-count"),
+            RString::from("rs-counter-plugin-example"),
+        )
+        .await;
+    }
+    .into_ffi()
 }
 
 #[sabi_extern_fn]
-pub fn handler(_: REvent, _: RCore_CTO) -> FfiFuture<()> {
-    async move {}.into_ffi()
+pub fn handler(event: REvent, core: RCore_CTO<'static, 'static>) -> FfiFuture<()> {
+    async move {
+        if event.event_name().to_string().as_str() == "rs-count" {
+            let state = core.state().await;
+            let count = state
+                .lookup("rs-count")
+                .and_then(|v| ROption::from(v.int()))
+                .unwrap_or(0);
+            core.send_modification(
+                RCoreModification::default().add_field("foobar", RValue::new_int(count + 1)),
+            )
+            .await;
+        }
+    }
+    .into_ffi()
 }
