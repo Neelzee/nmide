9fee65200c3b9f487e934658f8831160067ac83b
Neelzee <nilsien2001@gmail.com>
2025-06-01 11:21:58 +0200

    chore: Removed Apollo from draft names

--- draftName.sh
+++ draftName.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-names=("Artemis" "Apollo" "Gemini" "Mercury" "Orion" "Voyager" "Pioneer" "Mariner" "Juno" "Galileo" "Hubble" "Kepler" "Spitzer" "Dragonfly" "Perseverance" "Curiosity" "Opportunity" "Spirit" "Starliner" "Skylab" "Insight" "TESS" "Lucy" "Osiris-Rex")
+names=("Artemis" "Gemini" "Mercury" "Orion" "Voyager" "Pioneer" "Mariner" "Juno" "Galileo" "Hubble" "Kepler" "Spitzer" "Dragonfly" "Perseverance" "Curiosity" "Opportunity" "Spirit" "Starliner" "Skylab" "Insight" "TESS" "Lucy" "Osiris-Rex")
 
 if [ -n "$1" ]; then
   seed="$1"
