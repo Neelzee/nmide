f62e09a48f53f198e2027790116628273c043b78
Nils Michael Fitjar <nils.fitjar@tryg.no>
2025-09-01 08:25:09 +0200

    fix(server): Corrected dockerBuild script

--- core/dockerBuild.sh
+++ core/dockerBuild.sh
@@ -6,6 +6,7 @@ bash ./buildModules.sh
 popd
 pushd .
 cd src-tauri
+cargo build --release --features server
 docker build . -t nmide-server
 docker image tag nmide-server neelzee/nmide-server:latest
 docker image push neelzee/nmide-server:latest
