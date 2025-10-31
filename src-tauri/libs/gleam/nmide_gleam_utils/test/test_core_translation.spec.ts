6c2c8dfc5060f56f69f770bfa515ed66cca2ca93
Neelzee <nilsien2001@gmail.com>
2025-10-31 11:31:49 +0100

    test(nmide_gleam_utils): Addes unit test

--- src-tauri/libs/gleam/nmide_gleam_utils/test/test_core_translation.spec.ts
+++ src-tauri/libs/gleam/nmide_gleam_utils/test/test_core_translation.spec.ts
@@ -1,11 +1,10 @@
 import { DebugCore } from "@nmide/js-utils";
 import { test, expect } from "vitest";
 import { from_js_core_to_gleam_core } from "../build/dev/javascript/nmide_gleam_utils/gleam/decoder/core.mjs";
-import { Core as GleamCore } from "../build/dev/javascript/nmide_gleam_utils/gleam/utils/core.mjs";
 
-test("DebugCore Translation", () => {
+test("DebugCore Translation has ui", () => {
   const debugcore = DebugCore();
   const gleam_core = from_js_core_to_gleam_core(debugcore);
   console.log(gleam_core);
-  expect(gleam_core).toBeInstanceOf(GleamCore);
+  expect(gleam_core).toHaveProperty("ui")
 });
